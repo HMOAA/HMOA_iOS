aabef87dba979214920aca00c483072866853233
leemhyungyu <lhglbkksj@naver.com>
2023-01-11 00:00:01 +0900

    [ADD] 프로젝트 생성

---
+++ HMOA_iOS/HMOA_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HMOA_iOS
+//
+//  Created by 임현규 on 2023/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
