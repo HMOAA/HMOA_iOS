ee4eee13ded2ba4fc1274da7833355e9e06fc3a7
leemhyungyu <lhglbkksj@naver.com>
2023-01-13 21:28:08 +0900

    [ADD] 기능 추가여

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/ViewController/MyPageViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/ViewController/MyPageViewController.swift
@@ -13,5 +13,6 @@ class MyPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
+        // TEST
     }
 }
