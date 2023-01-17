f1390dc3695058a25997e7415e429df88a0e24c8
leemhyungyu <lhglbkksj@naver.com>
2023-01-17 22:51:28 +0900

    [ADD] 뉴스레터 모아보기 Cell UI 구현

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/View/HomeTopCell.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/View/HomeTopCell.swift
@@ -5,4 +5,66 @@
 //  Created by 임현규 on 2023/01/16.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class HomeTopCell: UICollectionViewCell {
+    
+    // MARK: - identifier
+    static let identifier = "HomeTopCell"
+    
+    // MARK: - Properies
+    
+    lazy var newsImageView = UIImageView().then {
+        $0.image = UIImage(named: "newsImage")
+    }
+    
+    lazy var leftButton: UIButton = {
+        
+        let button = UIButton()
+        
+        button.setImage(UIImage(named: "leftButton"), for: .normal)
+        
+        return button
+        
+    }()
+    
+    lazy var rightButton: UIButton = {
+        let button = UIButton()
+        
+        button.setImage(UIImage(named: "rightButton"), for: .normal)
+        
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    override func layoutSubviews() {
+        configureUI()
+    }
+}
+
+// MARK: Functions
+
+extension HomeTopCell {
+    
+    func configureUI() {
+        [newsImageView, leftButton, rightButton] .forEach { addSubview($0) }
+        
+        newsImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+//            $0.width.equalTo(335)
+//            $0.height.equalTo(202)
+        }
+        
+        leftButton.snp.makeConstraints {
+            $0.centerY.equalTo(newsImageView)
+            $0.leading.equalTo(newsImageView.snp.leading).inset(14)
+        }
+        
+        rightButton.snp.makeConstraints {
+            $0.centerY.equalTo(leftButton)
+            $0.trailing.equalTo(newsImageView.snp.trailing).inset(14)
+        }
+    }
+}
--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/ViewController/HomeViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/ViewController/HomeViewController.swift
@@ -15,7 +15,7 @@ class HomeViewController: UIViewController {
         let layout = UICollectionViewCompositionalLayout { (sectionIndex, _) -> NSCollectionLayoutSection? in
             switch sectionIndex {
             case 0:
-                return self.homeCellCompositionalLayout()
+                return self.homeTopCellCompositionalLayout()
             default:
                 return self.homeCellCompositionalLayout()
             }
@@ -24,7 +24,9 @@ class HomeViewController: UIViewController {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         
         collectionView.register(HomeCell.self, forCellWithReuseIdentifier: HomeCell.identifier)
+        collectionView.register(HomeTopCell.self, forCellWithReuseIdentifier: HomeTopCell.identifier)
         collectionView.register(HomeCellHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HomeCellHeaderView.identifier)
+        collectionView.register(HomeTopCellFooterView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: HomeTopCellFooterView.identifier)
         return collectionView
     }()
     
@@ -39,19 +41,35 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController {
     
-//    func homeTopCellCompositionalLayout() -> NSCollectionLayoutSection {
-//
-//    }
+    func homeTopCellCompositionalLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(202))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(202))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let footerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(33))
+        let sectionFooter = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: footerSize, elementKind: UICollectionView.elementKindSectionFooter, alignment: .bottomLeading)
+        sectionFooter.contentInsets = NSDirectionalEdgeInsets(top: -50, leading: 0, bottom: 0, trailing: 0)
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 25, trailing: 0)
+        section.boundarySupplementaryItems = [ sectionFooter ]
+        section.orthogonalScrollingBehavior = .groupPaging
+
+        return section
+    }
     
     func homeCellCompositionalLayout() -> NSCollectionLayoutSection {
         let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(100), heightDimension: .absolute(150))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 1.5)
 
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.28), heightDimension: .absolute(170))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.29), heightDimension: .absolute(170))
+
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
         
-        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(30))
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(36))
         let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .topLeading)
         
         let section = NSCollectionLayoutSection(group: group)
@@ -69,9 +87,9 @@ extension HomeViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.top.bottom.equalTo(view.safeAreaLayoutGuide)
             $0.leading.equalToSuperview().offset(20)
-            $0.bottom.trailing.equalToSuperview()
+            $0.trailing.equalToSuperview()
         }
     }
 }
@@ -82,18 +100,30 @@ extension HomeViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let homeCell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCell.identifier, for: indexPath) as? HomeCell else { return UICollectionViewCell() }
         
-        homeCell.perfumeImageView.image = UIImage(named: "jomalon")
-        homeCell.perfumeTitleLabel.text = "조 말론 런던"
-        homeCell.perfumeInfoLabel.text = "우드 세이지 앤 씨 쏠트 코롱 100ml"
+        guard let homeTopCell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeTopCell.identifier, for: indexPath) as? HomeTopCell else { return UICollectionViewCell() }
         
-        print(homeCell.frame.size)
-        return homeCell
+        switch indexPath.section {
+        case 0:
+            return homeTopCell
+        default:
+            homeCell.perfumeImageView.image = UIImage(named: "jomalon")
+            homeCell.perfumeTitleLabel.text = "조 말론 런던"
+            homeCell.perfumeInfoLabel.text = "우드 세이지 앤 씨 쏠트 코롱 100ml"
+            return homeCell
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HomeCellHeaderView.identifier, for: indexPath) as? HomeCellHeaderView else { return UICollectionReusableView() }
+     
+        guard let footer = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: HomeTopCellFooterView.identifier, for: indexPath) as? HomeTopCellFooterView else { return UICollectionReusableView() }
         
-        return header
+        switch kind {
+        case UICollectionView.elementKindSectionFooter:
+            return footer
+        default:
+            return header
+        }
     }
 }
 
@@ -105,7 +135,7 @@ extension HomeViewController: UICollectionViewDataSource {
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 3
+        return 4
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
