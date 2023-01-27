05e0edc394eaeba1ef296374347a1aea370bf687
jihun32 <jihun32@naver.com>
2023-01-27 20:49:10 +0900

    [MOD] 코드 리뷰 참고하여 수정

--- HMOA_iOS/HMOA_iOS/Source/Extenstions/UITextField++Extenstions.swift
+++ HMOA_iOS/HMOA_iOS/Source/Extenstions/UITextField++Extenstions.swift
@@ -20,4 +20,22 @@ extension UITextField {
         self.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: width, height: 0.0))
         self.leftViewMode = .always
     }
+    
+    func setTextFieldUI(_ placeholder: String, leftPadding: CGFloat, isCapsule: Bool = true) {
+        self.backgroundColor = .customColor(.searchBarColor)
+        self.font = .customFont(.pretendard, 14)
+        self.placeholder = placeholder
+        self.addLeftPadding(leftPadding)
+        self.setPlaceholder(color: .black)
+
+        if isCapsule {
+            self.snp.makeConstraints { make in
+                make.height.equalTo(33)
+            }
+            
+            self.layer.cornerRadius = 33 / 2
+        }
+        
+        
+    }
 }
--- HMOA_iOS/HMOA_iOS/Source/Presentation/Login/ViewController/RegisterViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Login/ViewController/RegisterViewController.swift
@@ -20,32 +20,32 @@ class RegisterViewController: UIViewController {
         $0.text = "회원가입"
     }
     
-    lazy var entireStackView = UIStackView().then {
-        setStackView($0, spacing: 29)
+    let entireStackView = UIStackView().then {
+        $0.setStackViewUI(spacing: 29)
     }
     
-    lazy var nameStackView = UIStackView().then {
-        setStackView($0, spacing: 7)
+    let nameStackView = UIStackView().then {
+        $0.setStackViewUI(spacing: 7)
     }
-    lazy var nameLabel = UILabel().then {
-        setLabel($0, "이름")
+    let nameLabel = UILabel().then {
+        $0.setLabelUI("이름")
     }
-    lazy var nameTextField = UITextField().then {
-        setTextField($0, "이름")
+    let nameTextField = UITextField().then {
+        $0.setTextFieldUI("이름", leftPadding: 14)
     }
     
-    lazy var idStackView = UIStackView().then {
-        setStackView($0, spacing: 7)
+    let idStackView = UIStackView().then {
+        $0.setStackViewUI(spacing: 7)
     }
-    lazy var idLabel = UILabel().then {
-        setLabel($0, "아이디")
+    let idLabel = UILabel().then {
+        $0.setLabelUI("아이디")
     }
-    lazy var idHorizontalStackView = UIStackView().then {
-        setStackView($0, spacing: 9, axis: .horizontal)
+    let idHorizontalStackView = UIStackView().then {
+        $0.setStackViewUI(spacing: 9, axis: .horizontal)
     }
-    lazy var idTextField = UITextField().then {
+    let idTextField = UITextField().then {
         $0.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-        setTextField($0, "아이디")
+        $0.setTextFieldUI("아이디", leftPadding: 14)
     }
     
     let duplicateCheckButton = UIButton().then {
@@ -58,23 +58,23 @@ class RegisterViewController: UIViewController {
     }
     
     lazy var pwStackView = UIStackView().then {
-        setStackView($0, spacing: 7)
+        $0.setStackViewUI(spacing: 7)
     }
-    lazy var pwLabel = UILabel().then {
-        setLabel($0, "비밀번호")
+    let pwLabel = UILabel().then {
+        $0.setLabelUI("비밀번호")
     }
-    lazy var pwTextField = UITextField().then {
-        setTextField($0, "비밀번호 (영어소문자+숫자 8자리 이상)")
+    let pwTextField = UITextField().then {
+        $0.setTextFieldUI("비밀번호 (영어소문자+숫자 8자리 이상)", leftPadding: 14)
     }
     
     lazy var pwCheckStackView = UIStackView().then {
-        setStackView($0, spacing: 7)
+        $0.setStackViewUI(spacing: 7)
     }
-    lazy var pwCheckLabel = UILabel().then {
-        setLabel($0, "비밀빈허 재확인")
+    let pwCheckLabel = UILabel().then {
+        $0.setLabelUI("비밀빈호 재확인")
     }
-    lazy var pwCheckTextField = UITextField().then {
-        setTextField($0, "비밀번호 재확인")
+    let pwCheckTextField = UITextField().then {
+        $0.setTextFieldUI("비밀번호 재확인", leftPadding: 14)
     }
     
     let registerButton = UIButton().then {
@@ -126,39 +126,14 @@ class RegisterViewController: UIViewController {
             make.leading.trailing.equalToSuperview().inset(20)
             make.top.equalTo(registerLabel.snp.bottom).offset(16)
         }
-
+        
         registerButton.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(66)
             make.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
-    
-    private func setTextField(_ textField: UITextField, _ placeholder: String) {
-        textField.backgroundColor = .customColor(.searchBarColor)
-        textField.font = .customFont(.pretendard, 14)
-        textField.placeholder = placeholder
-        textField.addLeftPadding(14)
-        textField.setPlaceholder(color: .black)
-
-        textField.snp.makeConstraints { make in
-            make.height.equalTo(33)
-        }
-        textField.layer.cornerRadius = 33 / 2
-        
-    }
-    
-    private func setLabel(_ label: UILabel, _ text: String) {
-        label.font = .customFont(.pretendard, 14)
-        label.text = text
-    }
-    
-    private func setStackView(_ stackView: UIStackView, spacing: CGFloat, axis: NSLayoutConstraint.Axis = .vertical) {
-        stackView.distribution = .fill
-        stackView.spacing = spacing
-        stackView.axis = axis
-    }
-    
+       
     //MARK: - Function
     @objc func didTapRegisterButton(_ sender: UIButton) {
         dismiss(animated: true)
