e4cd463a290da721ed42576ee8636a5505b0eafa
leemhyungyu <lhglbkksj@naver.com>
2023-02-23 02:08:16 +0900

    [MOD] 향수 상세보기 페이지 네비게이션 아이템 rx으로 수정

--- HMOA_iOS/HMOA_iOS/Source/Extenstions/UINavigationItem++Extenstions.swift
+++ HMOA_iOS/HMOA_iOS/Source/Extenstions/UINavigationItem++Extenstions.swift
@@ -25,24 +25,14 @@ extension UINavigationItem {
         
         return barButtonItem
     }
-}
     
-//    func makeTextButtonItem(_ target: Any?, action: Selector, title: String) -> UIBarButtonItem {
-//
-//        let button = UIButton().then {
-//            $0.setTitle(title, for: .normal)
-//            $0.titleLabel?.font = .customFont(.pretendard, 16)
-//            $0.setTitleColor(.black, for: .normal)
-//            $0.addTarget(target, action: action, for: .touchUpInside)
-//            $0.tintColor = .black
-//        }
-//
-//        let barButtonItem = UIBarButtonItem(customView: button)
-//
-//        barButtonItem.customView?.snp.makeConstraints {
-//            $0.width.equalTo(28)
-//            $0.height.equalTo(16)
-//        }
-//
-//        return barButtonItem
-//    }}
+    func makeImageButtonItem(_ button: UIButton) -> UIBarButtonItem {
+        let barButtonItem = UIBarButtonItem(customView: button)
+        
+        barButtonItem.customView?.snp.makeConstraints {
+            $0.width.height.equalTo(24)
+        }
+        
+        return barButtonItem
+    }
+}
