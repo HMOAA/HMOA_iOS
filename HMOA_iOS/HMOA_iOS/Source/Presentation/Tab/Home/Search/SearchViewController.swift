bf12a639f5093b8c5b5703b10972caf8584822fb
leemhyungyu <lhglbkksj@naver.com>
2023-02-23 18:51:56 +0900

    [ADD] 검색 페이지 UI 작업

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Search/SearchViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Search/SearchViewController.swift
@@ -9,7 +9,42 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    
+    // MARK: - UI Component
+    
+    let backButton = UIButton().makeImageButton(UIImage(named: "backButton")!)
+    
+    let searchBar = UISearchBar().then {
+        $0.showsBookmarkButton = true
+        $0.setImage(UIImage(named: "clearButton"), for: .clear, state: .normal)
+        $0.setImage(UIImage(named: "search")?.withTintColor(.customColor(.gray3)), for: .bookmark, state: .normal)
+        $0.searchTextField.leftView = UIView()
+        $0.searchTextField.backgroundColor = .white
+        $0.searchTextField.textAlignment = .left
+        $0.searchTextField.font = .customFont(.pretendard_light, 16)
+        $0.placeholder = "제품/브랜드/키워드 검색"
+    }
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+        configureNavigationBar()
+    }
+}
+
+extension SearchViewController {
+    
+    func configureUI() {
+        view.backgroundColor = .white
+    }
+    
+    func configureNavigationBar() {
+     
+        let backButtonItem = UIBarButtonItem(customView: backButton)
+                
+        self.navigationItem.leftBarButtonItems = [backButtonItem]
+        
+        self.navigationItem.titleView = searchBar
     }
 }
