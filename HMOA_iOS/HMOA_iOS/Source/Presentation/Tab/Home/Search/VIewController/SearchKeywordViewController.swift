ab99da93d3f19011eb78beb7fd5137bebac53ca3
leemhyungyu <lhglbkksj@naver.com>
2023-02-27 22:15:56 +0900

    [Feat] 키워드, 검색목록, 결과 VC 추가

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Search/VIewController/SearchKeywordViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchKeywordViewController.swift
+//  HMOA_iOS
+//
+//  Created by 임현규 on 2023/02/27.
+//
+
+import UIKit
+
+class SearchKeywordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
