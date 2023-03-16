d3ae5dbd99551ad4592b59a94d490a1fe051d682
leemhyungyu <lhglbkksj@naver.com>
2023-03-16 19:20:46 +0900

    [Feat] 홈 화면에서 brandSearchButton 클릭시 해당 VC으로 이동

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Brand/VIewController/BrandSearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BrandSearchViewController.swift
+//  HMOA_iOS
+//
+//  Created by 임현규 on 2023/03/16.
+//
+
+import UIKit
+
+class BrandSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
