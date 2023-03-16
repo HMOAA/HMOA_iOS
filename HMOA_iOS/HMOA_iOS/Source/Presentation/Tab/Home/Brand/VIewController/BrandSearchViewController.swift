cdcbd1d2fe9d87083101bd05d980df18ea87fff1
leemhyungyu <lhglbkksj@naver.com>
2023-03-16 19:23:36 +0900

    [Feat] 브랜드 검색 페이지 NavigationBar UI 구현

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Brand/VIewController/BrandSearchViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Brand/VIewController/BrandSearchViewController.swift
@@ -9,7 +9,47 @@ import UIKit
 
 class BrandSearchViewController: UIViewController {
 
+    // MARK: - UI Component
+    lazy var backButton = UIButton().makeImageButton(UIImage(named: "backButton")!)
+    
+    lazy var searchBar = UISearchBar().then {
+        $0.showsBookmarkButton = true
+        $0.setImage(UIImage(named: "clearButton"), for: .clear, state: .normal)
+        $0.setImage(UIImage(named: "search")?.withTintColor(.customColor(.gray3)), for: .bookmark, state: .normal)
+        $0.searchTextField.leftView = UIView()
+        $0.searchTextField.backgroundColor = .white
+        $0.searchTextField.textAlignment = .left
+        $0.searchTextField.font = .customFont(.pretendard_light, 16)
+        $0.placeholder = "제품/브랜드/키워드 검색"
+    }
+    
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+        configureNavigationBar()
+    }
+}
+
+extension BrandSearchViewController {
+    
+    // MARK: - Configure
+    
+    func configureUI() {
+        view.backgroundColor = .white
+    }
+    
+    func configureNavigationBar() {
+     
+        let backButtonItem = UIBarButtonItem(customView: backButton)
+        
+        let searchBarWrapper = SearchBarContainerView(customSearchBar: searchBar)
+        
+        searchBarWrapper.frame = CGRect(x: 0, y: 0, width: self.navigationController!.view.frame.size.width - 42, height: 30)
+        
+        self.navigationItem.leftBarButtonItems = [backButtonItem]
+        
+        self.navigationItem.titleView = searchBarWrapper
     }
 }
