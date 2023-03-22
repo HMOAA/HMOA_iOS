7d00c57dc3a6a4c3579b394e668f47e53a92df2f
leemhyungyu <lhglbkksj@naver.com>
2023-03-22 21:58:44 +0900

    [Feat] dropdown UI 및 기능 구현

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/BrandDetail/View/BrandDetailHeaderView.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/BrandDetail/View/BrandDetailHeaderView.swift
@@ -11,6 +11,7 @@ import Then
 import RxSwift
 import RxCocoa
 import ReactorKit
+import DropDown
 
 class BrandDetailHeaderView: UICollectionReusableView, View {
     typealias Reactor = BrandDetailHeaderReactor
@@ -46,18 +47,22 @@ class BrandDetailHeaderView: UICollectionReusableView, View {
         $0.setImage(UIImage(named: "bottomHeart"), for: .normal)
     }
     
+    
     // TODO: - 드롭다운 라이브러리 사용해서 추후에 구현
-    lazy var filterButton = UIButton().then {
+    lazy var sortButton = UIButton().then {
         $0.titleLabel?.font = .customFont(.pretendard_light, 12)
         $0.setTitleColor(.black, for: .normal)
-        $0.setTitle("최신순", for: .normal)
+//        $0.setTitle("최신순", for: .normal)
     }
+    
+    lazy var sortDropDown = DropDown()
     
     // MARK: - init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureUI()
+        configureDropDown()
     }
     
     required init?(coder: NSCoder) {
@@ -70,6 +75,20 @@ extension BrandDetailHeaderView {
     // MARK: - bind
     func bind(reactor: BrandDetailHeaderReactor) {
         
+        // MARK: - Action
+        
+        // 정렬 버튼 클릭
+        sortButton.rx.tap
+            .map { Reactor.Action.didTapSortButton }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        // DropDown 아이템 클릭
+        sortDropDown.rx.selectionAction
+            .onNext { (index, string) in
+                reactor.action.onNext(.didTapSortDropDown(index, string))
+            }
+
         // MARK: - State
         
         // 한글 브랜드명
@@ -92,6 +111,24 @@ extension BrandDetailHeaderView {
             .distinctUntilChanged()
             .bind(to: likeButton.rx.isSelected)
             .disposed(by: disposeBag)
+        
+        // DropDown 보여주기
+        reactor.state
+            .map { $0.isPresentDropDown }
+            .distinctUntilChanged()
+            .filter { $0 }
+            .map { _ in }
+            .bind(onNext: {self.sortDropDown.show()})
+            .disposed(by: disposeBag)
+        
+        // sortButton title바꾸기
+        reactor.state
+            .map { $0.nowSortType }
+            .distinctUntilChanged()
+            .bind(onNext: {
+                self.sortButton.setTitle($0, for: .normal)
+            })
+            .disposed(by: disposeBag)
     }
     
     // MARK: - Configure
@@ -99,7 +136,7 @@ extension BrandDetailHeaderView {
     func configureUI() {
         
         [   brandInfoView,
-            filterButton
+            sortButton
         ]   .forEach { addSubview($0) }
         
         [   englishLabel,
@@ -135,9 +172,21 @@ extension BrandDetailHeaderView {
             $0.width.height.equalTo(100)
         }
         
-        filterButton.snp.makeConstraints {
+        sortButton.snp.makeConstraints {
             $0.top.equalTo(brandInfoView.snp.bottom).offset(16)
-            $0.trailing.equalToSuperview().inset(21)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.width.equalTo(80)
         }
+    }
+    
+    func configureDropDown() {
+        sortDropDown.dataSource = ["추천순", "인기순", "최신순"]
+        sortDropDown.anchorView = self.sortButton
+        sortDropDown.bottomOffset = CGPoint(x: -13, y: 30)
+        
+        sortDropDown.textFont = .customFont(.pretendard_light, 12)
+        sortDropDown.selectedTextColor = .black
+        sortDropDown.textColor = .customColor(.gray3)
+        sortDropDown.selectRow(at: 0)
     }
 }
