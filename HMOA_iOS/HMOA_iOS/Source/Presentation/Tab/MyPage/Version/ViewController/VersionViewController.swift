23ce594dc15eee85c1b2d28d8eb0413a382ae34e
leemhyungyu <lhglbkksj@naver.com>
2023-03-24 22:29:29 +0900

    [Feat] 마이페이지 클릭시 이동하는 ViewController 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/Version/ViewController/VersionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VersionViewController.swift
+//  HMOA_iOS
+//
+//  Created by 임현규 on 2023/03/24.
+//
+
+import UIKit
+
+class VersionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setBackItemNaviBar("버전 정보")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
