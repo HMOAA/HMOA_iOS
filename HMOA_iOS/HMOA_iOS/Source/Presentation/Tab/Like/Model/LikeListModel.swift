4952fb1630abc4745f74f79ba1a85e31b6b0b983
jihun32 <jihun32@naver.com>
2023-04-04 10:26:06 +0900

    [Feat] LikeVC 향수 터치 화면 전환 구현

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Like/Model/LikeListModel.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Like/Model/LikeListModel.swift
@@ -8,39 +8,40 @@
 import Foundation
 
 struct ListData {
+    let id: Int
     let imgName: String
 }
 
 
 extension ListData {
     static let items: [ListData] = [
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon"),
-    ListData(imgName: "jomalon")
+        ListData(id: 1, imgName: "jomalon"),
+        ListData(id: 2, imgName: "jomalon"),
+        ListData(id: 3, imgName: "jomalon"),
+        ListData(id: 4, imgName: "jomalon"),
+        ListData(id: 5, imgName: "jomalon"),
+        ListData(id: 6, imgName: "jomalon"),
+        ListData(id: 7, imgName: "jomalon"),
+        ListData(id: 8, imgName: "jomalon"),
+        ListData(id: 9, imgName: "jomalon"),
+        ListData(id: 10, imgName: "jomalon"),
+        ListData(id: 11, imgName: "jomalon"),
+        ListData(id: 12, imgName: "jomalon"),
+        ListData(id: 13, imgName: "jomalon"),
+        ListData(id: 14, imgName: "jomalon"),
+        ListData(id: 15, imgName: "jomalon"),
+        ListData(id: 16, imgName: "jomalon"),
+        ListData(id: 17, imgName: "jomalon"),
+        ListData(id: 18, imgName: "jomalon"),
+        ListData(id: 19, imgName: "jomalon"),
+        ListData(id: 20, imgName: "jomalon"),
+        ListData(id: 21, imgName: "jomalon"),
+        ListData(id: 22, imgName: "jomalon"),
+        ListData(id: 23, imgName: "jomalon"),
+        ListData(id: 24, imgName: "jomalon"),
+        ListData(id: 25, imgName: "jomalon"),
+        ListData(id: 26, imgName: "jomalon"),
+        ListData(id: 27, imgName: "jomalon"),
+        ListData(id: 28, imgName: "jomalon")
     ]
 }
