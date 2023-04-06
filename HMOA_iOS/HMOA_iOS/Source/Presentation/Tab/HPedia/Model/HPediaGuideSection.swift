d8498d98e7908787d95b0814b422ced3bca9612e
jihun32 <jihun32@naver.com>
2023-04-06 16:52:08 +0900

    [Feat] 리뷰 반영

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/HPedia/Model/HPediaGuideSection.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  HMOA_iOS
+//
+//  Created by 정지훈 on 2023/04/05.
+//
+
+import Foundation
---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/HPedia/View/GuideCell.swift
@@ -0,0 +1,12 @@
+//
+//  GuideCell.swift
+//  HMOA_iOS
+//
+//  Created by 정지훈 on 2023/04/04.
+//
+
+import UIKit
+
+class GuideCell: UICollectionViewCell {
+    
+}
