c04d482ebffd48cf7ffa28d1f58e9c3002005830
jihun32 <jihun32@naver.com>
2023-04-10 09:13:25 +0900

    [Feat] git ignore API KEY 추가

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Login/Key/Key.swift
@@ -0,0 +1,13 @@
+//
+//  Key.swift
+//  HMOA_iOS
+//
+//  Created by 정지훈 on 2023/04/10.
+//
+
+import Foundation
+
+
+struct Key {
+    static let GOOGLE_CLIENT_ID = "788055646907-v9chjefcdm4vs2p5c6utgvmutbp0lrv4.apps.googleusercontent.com"
+}
