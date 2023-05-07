edaf604729ff2b03937bfd5d8ef6902927813d22
leemhyungyu <lhglbkksj@naver.com>
2023-05-07 19:55:48 +0900

    [Feat] 닉네임 cell클릭시 해당 VC으로 이동

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/MyProfile/Viewcontroller/ChangeNicknameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChangeNicknameViewController.swift
+//  HMOA_iOS
+//
+//  Created by 임현규 on 2023/05/07.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+
+class ChangeNicknameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
