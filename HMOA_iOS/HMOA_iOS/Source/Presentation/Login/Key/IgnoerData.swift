2af8813e5f7ed43e4c9064b10ebb10c87d1bde23
jihun32 <jihun32@naver.com>
2023-05-29 21:18:54 +0900

    [Feat] gitignore수정

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Login/Key/IgnoerData.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Login/Key/IgnoerData.swift
@@ -9,10 +9,10 @@ import Foundation
 
 
 struct Key {
-    static let GOOGLE_CLIENT_ID = "788055646907-v9chjefcdm4vs2p5c6utgvmutbp0lrv4.apps.googleusercontent.com"
+    static let GOOGLE_CLIENT_ID = ""
     
     static let KAKAO_NATIVE_APP_KEY =
-        "4af51404e2d5676f99bf8a1e7c16fad7"
+        ""
 }
 
 struct baseURL {
