c10978221817664dde1ad9ae8385e36d3becc289
leemhyungyu <lhglbkksj@naver.com>
2023-07-02 22:11:12 +0900

    [Feat] 마이페이지 - 프로필 이미지 VC UI 임시 구현

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/MyProfile/Viewcontroller/ChangeProfileImageViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ChangeProfileImageViewController.swift
+//  HMOA_iOS
+//
+//  Created by 임현규 on 2023/07/02.
+//
+
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+import ReactorKit
+
+class ChangeProfileImageViewController: UIViewController {
+
+    // MARK: - UI Component
+    
+    lazy var profileImageView: UIImageView = UIImageView().then {
+        $0.backgroundColor = .customColor(.gray3)
+        $0.layer.cornerRadius = 25
+    }
+    
+    lazy var showAlbumButton: UIButton = UIButton().then {
+        var configure = UIButton.Configuration.plain()
+        var attributedString = AttributedString.init("사진 선택")
+        attributedString.font = .customFont(.pretendard_light, 14)
+        
+        configure.baseForegroundColor = .white
+        configure.attributedTitle = attributedString
+        
+        $0.configuration = configure
+        $0.backgroundColor = .black
+    }
+    
+    lazy var changeButton: UIButton = UIButton().then {
+        $0.setProfileChangeBottomView()
+    }
+    
+    // MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setBackItemNaviBar("프로필 이미지")
+        configureUI()
+    }
+}
+
+extension ChangeProfileImageViewController {
+    
+    func configureUI() {
+        
+        view.backgroundColor = .white
+        
+        [
+            profileImageView,
+            showAlbumButton,
+            changeButton
+        ]   .forEach { view.addSubview($0) }
+        
+        profileImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.width.height.equalTo(50)
+        }
+        
+        showAlbumButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(profileImageView.snp.bottom).offset(10)
+            $0.width.equalTo(100)
+        }
+        
+        changeButton.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalTo(80)
+        }
+    }
+}
