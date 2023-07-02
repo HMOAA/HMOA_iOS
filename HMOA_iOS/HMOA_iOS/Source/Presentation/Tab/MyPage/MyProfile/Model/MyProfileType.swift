ae3a352b0078410d0d26258fae90ed0cff3e87e5
leemhyungyu <lhglbkksj@naver.com>
2023-07-02 21:17:52 +0900

    [Feat] 마이페이지 > 내 정보관리 - 프로필 이미지 섹션 추가

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/MyProfile/Model/MyProfileType.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/MyProfile/Model/MyProfileType.swift
@@ -8,12 +8,15 @@
 import Foundation
 
 enum MyProfileType: Int, CaseIterable {
+    case profileImage
     case nickname
     case year
     case sex
     
     var title: String {
         switch self {
+        case .profileImage:
+            return "프로필 이미지"
         case .nickname:
             return "닉네임"
         case .year:
