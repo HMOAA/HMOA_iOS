2e9d2f1ea90edcd9db62f059b3cab2bd3200a5b9
jihun32 <jihun32@naver.com>
2023-08-01 15:06:53 +0900

    [Feat] BrandSearch Model 변경

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/BrandSearch/Model/Brand.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/BrandSearch/Model/Brand.swift
@@ -2,7 +2,56 @@
 //  Brand.swift
 //  HMOA_iOS
 //
-//  Created by 정지훈 on 2023/07/21.
+//  Created by 임현규 on 2023/03/17.
 //
 
-import Foundation
+struct BrandList: Equatable, Hashable {
+    var consonant: Int
+    var brands: [Brand]
+}
+
+struct Brand: Equatable, Hashable, Codable {
+    let brandId: Int
+    let brandImageUrl: String
+    let brandName: String
+    let englishName: String
+}
+
+extension BrandList {
+    
+    var section: BrandListSection {
+        switch self.consonant {
+        case 1:
+            return BrandListSection.first(self.brands.map { BrandCell.BrandItem($0) })
+        case 2:
+            return BrandListSection.second(self.brands.map { BrandCell.BrandItem($0) })
+        case 3:
+            return BrandListSection.third(self.brands.map { BrandCell.BrandItem($0) })
+
+        case 4:
+            return BrandListSection.fourth(self.brands.map { BrandCell.BrandItem($0) })
+        case 5:
+            return BrandListSection.fifth(self.brands.map { BrandCell.BrandItem($0) })
+        case 6:
+            return BrandListSection.sixth(self.brands.map { BrandCell.BrandItem($0) })
+        case 7:
+            return BrandListSection.seventh(self.brands.map { BrandCell.BrandItem($0) })
+        case 8:
+            return BrandListSection.eighth(self.brands.map { BrandCell.BrandItem($0) })
+        case 9:
+            return BrandListSection.ninth(self.brands.map { BrandCell.BrandItem($0) })
+        case 10:
+            return BrandListSection.tenth(self.brands.map { BrandCell.BrandItem($0) })
+        case 11:
+            return BrandListSection.eleventh(self.brands.map { BrandCell.BrandItem($0) })
+        case 12:
+            return BrandListSection.twelfth(self.brands.map { BrandCell.BrandItem($0) })
+        case 13:
+            return BrandListSection.thirteenth(self.brands.map { BrandCell.BrandItem($0) })
+        case 14:
+            return BrandListSection.fourtheenth(self.brands.map { BrandCell.BrandItem($0) })
+        default:
+            return BrandListSection.first([])
+        }
+    }
+}
