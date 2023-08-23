41e96242baa9850cfe4a8cb800b8252c4aa5b45a
jihun32 <jihun32@naver.com>
2023-08-23 19:38:20 +0900

    [Feat] 이외 페이지들 presentDetailVC 임시 보류

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Detail/View/PerfumeInfoCell.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Detail/View/PerfumeInfoCell.swift
@@ -10,6 +10,7 @@ import SnapKit
 import Then
 import ReactorKit
 import RxCocoa
+import Kingfisher
 import RxSwift
 
 class PerfumeInfoCell: UICollectionViewCell, View {
@@ -44,36 +45,24 @@ extension PerfumeInfoCell {
     // MARK: - Bind
     
     func bind(reactor: PerfumeInfoViewReactor) {
-        perfumeInfoView.perfumeImageView.image = reactor.currentState.perfumeImage
-        perfumeInfoView.titleKoreanLabel.text = reactor.currentState.koreanName
-        perfumeInfoView.titleEnglishLabel.text = reactor.currentState.englishName
-        // 나중에 수정
-//        perfumeInfoView.keywordTagListView.addTags(reactor.currentState.category)
-        perfumeInfoView.priceLabel.text = reactor.currentState.price.numberFormatterToWon()
-        perfumeInfoView.ageLabel.text = "\(reactor.currentState.age)"
-        perfumeInfoView.gendarLabel.text = reactor.currentState.gender
-        perfumeInfoView.productInfoContentLabel.text = reactor.currentState.productInfo
-        perfumeInfoView.topNote.nameLabel.text = reactor.currentState.topTasting
-        perfumeInfoView.heartNote.nameLabel.text = reactor.currentState.heartTasting
-        perfumeInfoView.baseNote.nameLabel.text = reactor.currentState.baseTasting
-        
+
         // MARK: - Ation
-        
+
         // 향수 좋아요 버튼 클릭
         perfumeInfoView.perfumeLikeButton.rx.tap
             .map { Reactor.Action.didTapPerfumeLikeButton }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
-        
+
         // 향수 브랜드 좋아요 버튼 클릭
         perfumeInfoView.brandView.likeButton.rx.tap
             .do(onNext: { print("Clicked")} )
             .map { Reactor.Action.didTapBrandLikeButton }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
-        
+
         // MARK: - State
-        
+
         // 향수 좋아요 상태 변경
         reactor.state
             .map { $0.isLikePerfume }
@@ -81,14 +70,14 @@ extension PerfumeInfoCell {
             .bind(to:
                     perfumeInfoView.perfumeLikeButton.rx.isSelected)
             .disposed(by: disposeBag)
-        
+
         // 향수 브랜드 좋아요 상태 변경
         reactor.state
             .map { $0.isLikeBrand }
             .distinctUntilChanged()
             .bind(to: perfumeInfoView.brandView.likeButton.rx.isSelected)
             .disposed(by: disposeBag)
-        
+
         // 향수 좋아요 개수 변경
         reactor.state
             .map { $0.likeCount }
@@ -116,16 +105,13 @@ extension PerfumeInfoCell {
         return attri
     }
     
-    func updateCell(_ item: PerfumeDetail) {
-        perfumeInfoView.perfumeImageView.image = item.perfumeImage
+    func updateCell(_ item: Detail, _ image: String) {
+        perfumeInfoView.perfumeImageView.kf.setImage(with: URL(string: image)!)
         perfumeInfoView.titleKoreanLabel.text = item.koreanName
         perfumeInfoView.titleEnglishLabel.text = item.englishName
         perfumeInfoView.priceLabel.text = "\(item.price)"
-        perfumeInfoView.ageLabel.text = "\(item.age)"
-        perfumeInfoView.gendarLabel.text = item.gender
-        perfumeInfoView.productInfoContentLabel.text = item.productInfo
-        perfumeInfoView.topNote.nameLabel.text = item.topTasting
-        perfumeInfoView.heartNote.nameLabel.text = item.heartTasting
-        perfumeInfoView.baseNote.nameLabel.text = item.baseTasting
+        perfumeInfoView.topNote.nameLabel.text = item.topNote
+        perfumeInfoView.heartNote.nameLabel.text = item.heartNote
+        perfumeInfoView.baseNote.nameLabel.text = item.baseNote
     }
 }
