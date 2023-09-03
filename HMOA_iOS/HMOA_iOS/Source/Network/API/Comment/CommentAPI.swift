098838f0971745023c0e477a4f408e91883d7bd7
jihun32 <jihun32@naver.com>
2023-09-03 18:21:35 +0900

    [Feat] Comment 네트워킹 모델 구현

--- HMOA_iOS/HMOA_iOS/Source/Network/API/Comment/CommentAPI.swift
+++ HMOA_iOS/HMOA_iOS/Source/Network/API/Comment/CommentAPI.swift
@@ -6,7 +6,36 @@
 //
 
 import Foundation
+import RxSwift
 
 final class CommentAPI {
+    static func postComment(_ params: [String: String], _ id: Int) -> Observable<Response> {
+        guard let data = try? JSONSerialization.data(
+            withJSONObject: params,
+            options: .prettyPrinted) else { return .error(NetworkError.invalidParameters) }
+                
+        return networking(
+            urlStr: CommentAddress.postCommnet(id).url,
+            method: .post,
+            data: data,
+            model: Response.self)
+        
+    }
     
+    static func fetchCommentList(_ params: [String: Int], type: String) -> Observable<ResponseComment> {
+        guard let id = params["perfumeId"] else { return .error(NetworkError.invalidParameters) }
+        
+        //type에 따른 url 변경
+        var typeUrl: String
+        if type == "Latest" { typeUrl = "" }
+        else if type == "Like" { typeUrl = "/top" }
+        else { return .error(NetworkError.invalidURL) }
+        
+        return networking(
+            urlStr: CommentAddress.fetchCommentList(id).url + typeUrl,
+            method: .get,
+            data: nil,
+            model: ResponseComment.self,
+            query: params)
+    }
 }
