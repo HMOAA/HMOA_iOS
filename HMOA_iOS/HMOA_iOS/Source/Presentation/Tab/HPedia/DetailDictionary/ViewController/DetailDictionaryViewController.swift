6d663f03af5c38794fbbc91e91c8ccc31edd62a5
jihun32 <jihun32@naver.com>
2023-09-08 13:46:16 +0900

    [UI] DetailDictionary페이지 UI 구현

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/HPedia/DetailDictionary/ViewController/DetailDictionaryViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/HPedia/DetailDictionary/ViewController/DetailDictionaryViewController.swift
@@ -24,22 +24,12 @@ class DetailDictionaryViewController: UIViewController {
     }
     
     let explainLabel = UILabel().then {
-        $0.setLabelUI("샬라샬라", font: .pretendard_semibold, size: 16, color: .black)
+        $0.setLabelUI("사전 정의", font: .pretendard_semibold, size: 16, color: .black)
     }
     
     let contentLabel = UILabel().then {
         $0.numberOfLines = 0
-        $0.setLabelUI("""
-1
-2
-3
-4
-5
-6
-7
-8
-9
-""", font: .pretendard, size: 16, color: .black)
+        $0.setLabelUI("노트란 향에 대한 느낌을 말하는 것으로, 발향 순서에 따라 톱 노트, 미들 노트, 베이스 노트로 나뉜다. 이러한 분류는 여러 종류의 향을 배합할 때 각각의 느낌을 조화롭게 하기 위해 꼭 필요하다. 에센셜 오일 역시 한 가지만 사용하는 것보다는 두세 종류의 오일을 조합하는 경우가 많으므로, 각각의 오일이 갖고 있는 노트를 파악하고 있으면 자신이 좋아하는 향을 조합해 사용할 수 있다.", font: .pretendard, size: 16, color: .black)
     }
     
     override func viewDidLoad() {
@@ -83,7 +73,7 @@ class DetailDictionaryViewController: UIViewController {
         }
         
         contentLabel.snp.makeConstraints { make in
-            make.leading.equalToSuperview().inset(16)
+            make.trailing.leading.equalToSuperview().inset(16)
             make.top.equalTo(explainLabel.snp.bottom).offset(19)
         }
     }
