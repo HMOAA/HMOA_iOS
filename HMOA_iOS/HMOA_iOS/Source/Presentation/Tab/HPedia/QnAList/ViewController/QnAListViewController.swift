2243b5c150eda3a6e77988032dc483e2d2a8d6df
jihun32 <jihun32@naver.com>
2023-09-08 13:50:44 +0900

    [Feat] QnAList 폴더 설정

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/HPedia/QnAList/ViewController/QnAListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QnAListViewController.swift
+//  HMOA_iOS
+//
+//  Created by 정지훈 on 2023/09/08.
+//
+
+import UIKit
+
+class QnAListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
