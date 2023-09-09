7dd2559951cc9a5740b8be62fa2615a494c24477
jihun32 <jihun32@naver.com>
2023-09-09 18:56:56 +0900

    [Feat] 파일 추가

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/HPedia/QnAList/Reactor/QNAListReactor.swift
@@ -0,0 +1,8 @@
+//
+//  QNAListReactor.swift
+//  HMOA_iOS
+//
+//  Created by 정지훈 on 2023/09/09.
+//
+
+import Foundation
---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/HPedia/QnAList/View/QnAListHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  QnAListHeaderView.swift
+//  HMOA_iOS
+//
+//  Created by 정지훈 on 2023/09/08.
+//
+
+import UIKit
+
+class QnAListHeaderView: UICollectionReusableView {
+        
+}
