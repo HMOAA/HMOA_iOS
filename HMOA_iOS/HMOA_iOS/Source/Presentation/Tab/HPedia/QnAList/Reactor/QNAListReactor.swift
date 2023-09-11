3cc0f6e820cb479d2383060718b48781cff0b4da
jihun32 <jihun32@naver.com>
2023-09-11 21:38:29 +0900

    [Feat] QnAWrite페이지 UI 구현

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/HPedia/QnAList/Reactor/QNAListReactor.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/HPedia/QnAList/Reactor/QNAListReactor.swift
@@ -13,15 +13,21 @@ class QNAListReactor: Reactor {
     let initialState: State
     
     enum Action {
+        case viewWillAppear
         case didTapFloatingButton
+        case didTapRecommendButton
+        case didTapGiftButton
+        case didTapEtcButton
     }
     
     enum Mutation {
         case setIsTapFloatingButton(Bool)
+        case setSelectedCategory(String?)
     }
     
     
     struct State {
+        var selectedCategory: String? = nil
         var isFloatingButtonTap: Bool = false
         var items: [HPediaQnAData] = HPediaQnAData.list
     }
@@ -32,8 +38,25 @@ class QNAListReactor: Reactor {
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
+        case .viewWillAppear:
+            return .just(.setIsTapFloatingButton(false))
         case .didTapFloatingButton:
             return .just(.setIsTapFloatingButton(!currentState.isFloatingButtonTap))
+        case .didTapRecommendButton:
+            return .concat([
+                .just(.setSelectedCategory("추천")),
+                .just(.setSelectedCategory(nil))
+            ])
+        case .didTapGiftButton:
+            return .concat([
+                .just(.setSelectedCategory("선물")),
+                .just(.setSelectedCategory(nil))
+            ])
+        case .didTapEtcButton:
+            return .concat([
+                .just(.setSelectedCategory("기타")),
+                .just(.setSelectedCategory(nil))
+            ])
         }
     }
     
@@ -43,6 +66,8 @@ class QNAListReactor: Reactor {
         switch mutation {
         case .setIsTapFloatingButton(let isTap):
             state.isFloatingButtonTap = isTap
+        case .setSelectedCategory(let category):
+            state.selectedCategory = category
         }
         
         return state
