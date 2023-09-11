fc360420b9ec008f7953f9086362de023572d246
jihun32 <jihun32@naver.com>
2023-09-11 23:20:47 +0900

    [Feat] QnADetail 페이지 UI 구현

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/HPedia/QnADetail/Model/QnADetailSection.swift
@@ -0,0 +1,39 @@
+//
+//  QnADetailSection.swift
+//  HMOA_iOS
+//
+//  Created by 정지훈 on 2023/09/11.
+//
+
+
+enum QnADetailSection: Hashable {
+    case qnaPost([QnADetailSectionItem])
+    case comment([QnADetailSectionItem])
+}
+
+enum QnADetailSectionItem: Hashable {
+    case qnaPostCell(QnAData)
+    case commentCell(Comment)
+}
+
+extension QnADetailSection {
+    
+    var item: [QnADetailSectionItem] {
+        switch self {
+        case .qnaPost(let qnaPost):
+            return qnaPost
+        case .comment(let comment):
+            return comment
+        }
+    }
+}
+
+struct QnAData: Hashable {
+    let id: Int
+    let nickname: String
+    let day: Int
+    let title: String
+    let content: String
+    let profileImageUrl: String?
+}
+
