351c67a42a10dc7a1484573a479a02a2df060700
jihun32 <jihun32@naver.com>
2023-09-15 18:58:49 +0900

    [Feat] 계절 버튼 기능 구현

--- HMOA_iOS/HMOA_iOS/Source/Network/API/Detail/Evaluation/EvaluationAPI.swift
+++ HMOA_iOS/HMOA_iOS/Source/Network/API/Detail/Evaluation/EvaluationAPI.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+import RxSwift
+
+final class EvaluationAPI {
+    
+    
+    ///  계절 값 보내기
+    /// - Parameters:
+    ///   - id: 향수 id
+    ///   - weather:  [weather: 1~4]
+    ///     1: 봄, 2: 여름, 3: 가을, 4: 겨울
+    static func postSeason(id: String, weather: [String: Int]) -> Observable<Weather> {
+        
+        guard let data = try? JSONSerialization.data(
+                    withJSONObject: weather,
+                    options: .prettyPrinted)
+        else { return .error(NetworkError.invalidParameters) }
+        
+        return networking(
+            urlStr: EvaluationAddres.postWether(id).url,
+            method: .post,
+            data: data,
+            model: Weather.self)
+    }
+}
