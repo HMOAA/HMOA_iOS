f8aff4a57bbd0060531cabd8140a5cbd44112c9b
jihun32 <jihun32@naver.com>
2023-09-15 18:58:36 +0900

    [Feat] Button AttributedString 타이틀 설정 extension으로 수정

--- HMOA_iOS/HMOA_iOS/Source/Extenstions/AttributeString++Extension.swift
+++ HMOA_iOS/HMOA_iOS/Source/Extenstions/AttributeString++Extension.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension AttributedString {
+    
+    /// Button AttributeString 텍스트 설정
+    /// - Parameter text: title
+    func setButtonAttirbuteString(text: String, size: CGFloat, font: Fonts) -> AttributedString {
+        var attri = AttributedString.init(text)
+        attri.font = .customFont(font, size)
+        
+        return attri
+    }
+}
