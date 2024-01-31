e8841484b2ec8413c9570c3ae8903fe6d93f746b
jihun32 <jihun32@naver.com>
2024-01-31 21:54:37 +0900

    [Feat] 앱 재설치 시 로그아웃 적용

--- HMOA_iOS/HMOA_iOS/Source/Extenstions/UserDefaults++Extenstions.swift
+++ HMOA_iOS/HMOA_iOS/Source/Extenstions/UserDefaults++Extenstions.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension UserDefaults {
+    public static func isFirstLaunch() -> Bool {
+        let hasBeenLaunchedBeforeFlag = "hasBeenLaunchedBeforeFlag"
+        let isFirstLaunch = !UserDefaults.standard.bool(forKey: hasBeenLaunchedBeforeFlag)
+        if isFirstLaunch {
+            UserDefaults.standard.set(true, forKey: hasBeenLaunchedBeforeFlag)
+            UserDefaults.standard.synchronize()
+        }
+        return isFirstLaunch
+    }
+}
