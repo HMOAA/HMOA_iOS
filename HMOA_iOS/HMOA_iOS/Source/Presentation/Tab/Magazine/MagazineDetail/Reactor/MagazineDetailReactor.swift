c6c4cb729df1ec759758ec7780670178b43608f4
KwakDADA <down022@gmail.com>
2024-03-21 13:14:51 +0900

    [Feat] MagazineDetail info 섹션 네트워킹 구현

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Magazine/MagazineDetail/Reactor/MagazineDetailReactor.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Magazine/MagazineDetail/Reactor/MagazineDetailReactor.swift
@@ -13,11 +13,12 @@ import RxSwift
 class MagazineDetailReactor: Reactor {
     
     enum Action {
-        
+        // TODO: viewDidLoad case 작성 후 각 섹션 setUp method 호출
+        case viewDidLoad
     }
     
     enum Mutation {
-        
+        case setInfoItem(MagazineDetailItem)
     }
     
     struct State {
@@ -25,25 +26,50 @@ class MagazineDetailReactor: Reactor {
         var contentItems: [MagazineDetailItem] = []
         var likeItems: [MagazineDetailItem] = []
         var otherMagazineItems: [MagazineDetailItem] = []
+        
+        var magazineID: Int
     }
     
     let initialState: State
     
-    init() {
-        self.initialState = State()
+    init(_ id: Int) {
+        self.initialState = State(magazineID: id)
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
-            
+        case .viewDidLoad:
+            return .concat([
+                setUpMagazineInfoSection()
+            ])
         }
     }
     
     func reduce(state: State, mutation: Mutation) -> State {
         var state = state
         switch mutation {
+        case .setInfoItem(let item):
+            state.infoItems.append(item)
             
         }
         return state
+    }
+}
+
+extension MagazineDetailReactor {
+    func setUpMagazineInfoSection() -> Observable<Mutation> {
+        return MagazineAPI.fetchMagazineDetail(currentState.magazineID)
+            .catch { _ in .empty() }
+            .flatMap { magazineDetailData -> Observable<Mutation> in
+                
+                let title = magazineDetailData.title
+                let releasedDate = magazineDetailData.releasedDate
+                let viewCount = magazineDetailData.viewCount
+                let infoData = MagazineInfo(title: title, releasedDate: releasedDate, viewCount: viewCount)
+                
+                return .concat([
+                    .just(.setInfoItem(.info(infoData)))
+                ])
+            }
     }
 }
