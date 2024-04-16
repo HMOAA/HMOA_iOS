4ef82d45de6ef9a7e03246ad300fa02f7df2d834
jihun32 <jihun32@naver.com>
2024-04-16 20:05:35 +0900

    [Feat] 좋아요누른 향수 마이페이지 탭으로 이동

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/ViewModel/MyPageViewModel.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/ViewModel/MyPageViewModel.swift
@@ -10,21 +10,25 @@ import UIKit
 enum MyPageType: String, CaseIterable {
     case myProfile = "00"
     
-    case myLog = "20"
-    case myInformation = "21"
-     
-    case terms = "30"
-    case policy = "31"
-    case version = "32"
+    case myPerfume = "20"
     
-    case inquireAccount = "40"
-    case logout = "41"
-    case deleteAccount = "42"
+    case myLog = "30"
+    case myInformation = "31"
+     
+    case terms = "40"
+    case policy = "41"
+    case version = "42"
+    
+    case inquireAccount = "50"
+    case logout = "51"
+    case deleteAccount = "52"
 
     var title: String {
         switch self {
         case .myProfile:
             return ""
+        case .myPerfume:
+            return "나의 향수"
         case .myLog:
             return "내 활동"
         case .myInformation:
