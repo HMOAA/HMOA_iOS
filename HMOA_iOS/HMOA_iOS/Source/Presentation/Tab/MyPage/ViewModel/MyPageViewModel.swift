a9526c7b07e8f7a37b6926377691e7abcb488429
jihun32 <jihun32@naver.com>
2024-05-01 19:18:31 +0900

    [UI] 버전정보 수정

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/ViewModel/MyPageViewModel.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/ViewModel/MyPageViewModel.swift
@@ -37,7 +37,7 @@ enum MyPageType: String, CaseIterable {
         case .policy:
             return "개인정보 처리방침"
         case .version:
-            return "버전정보 1.0.3"
+            return "버전정보 1.0.4"
         case .inquireAccount:
             return "1대1 문의"
         case .logout:
