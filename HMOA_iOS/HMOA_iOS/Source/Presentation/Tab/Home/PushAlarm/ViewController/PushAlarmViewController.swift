e1f5fa105517fabb43618a207b74c1d5aad515bb
KwakDADA <down022@gmail.com>
2024-06-27 11:20:22 +0900

    [Feat] 알림리스트 Data Source, Snapshot 설정

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/PushAlarm/ViewController/PushAlarmViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/PushAlarm/ViewController/PushAlarmViewController.swift
@@ -1,27 +1,40 @@
 //
-//  NotificationViewController.swift
+//  PushAlarmViewController.swift
 //  HMOA_iOS
 //
 //  Created by 곽다은 on 6/17/24.
 //
 
 import UIKit
-import SnapKit
+
 import ReactorKit
 import RxCocoa
 import RxSwift
+import SnapKit
+import Then
 
 class PushAlarmViewController: UIViewController, View {
     
+    // MARK: - UI Components
+    
+    private lazy var pushAlarmTableView = UITableView(frame: .zero, style: .plain).then {
+        $0.register(PushAlarmCell.self, forCellReuseIdentifier: PushAlarmCell.identifier)
+    }
+    
     // MARK: - Properties
     
+    private var dataSource: UITableViewDiffableDataSource<PushAlarmSection, PushAlarmItem>?
+    private var sections = [PushAlarmSection]()
     var disposeBag = DisposeBag()
     
     // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUI()
+        setConstraints()
+        configureDatasource()
     }
     
     // MARK: - Bind
@@ -30,8 +43,48 @@ class PushAlarmViewController: UIViewController, View {
         
     }
     
-    func setUI() {
+    // MARK: - SetUp
+    
+    private func setUI() {
         view.backgroundColor = .white
         setBackBellNaviBar("H M O A")
+        view.addSubview(pushAlarmTableView)
+    }
+    
+    private func setConstraints() {
+        pushAlarmTableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    
+    // MARK: - Data Source Initialization
+    
+    private func configureDatasource() {
+        dataSource = .init(tableView: pushAlarmTableView,
+                           cellProvider: { (tableView, indexPath, item) -> UITableViewCell? in
+            let section = self.sections[indexPath.section]
+            switch section {
+            case .list:
+                let cell = tableView.dequeueReusableCell(
+                    withIdentifier: PushAlarmCell.identifier,
+                    for: indexPath) as! PushAlarmCell
+                
+                cell.configureCell()
+                
+                return cell
+            }
+        })
+        
+        // MARK: Initial Snapshot
+        
+        var initialSnapshot = NSDiffableDataSourceSnapshot<PushAlarmSection, PushAlarmItem>()
+        initialSnapshot.appendSections([.list])
+        initialSnapshot.appendItems([.pushAlarm(PushAlarm(ID: 1, category: "Event", content: "테스트", pushDate: "오늘", deepLink: "", isRead: false))], toSection: .list)
+        
+        sections = initialSnapshot.sectionIdentifiers
+        dataSource?.apply(initialSnapshot)
     }
 }
+
+
