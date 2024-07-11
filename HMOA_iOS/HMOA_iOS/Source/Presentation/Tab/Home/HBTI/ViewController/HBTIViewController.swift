4741dcf907d4e0c25e405b9484a4bf9961ab7029
KwakDADA <down022@gmail.com>
2024-07-11 15:31:31 +0900

    [Feat] HBTIVC 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/ViewController/HBTIViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HBTIViewController.swift
+//  HMOA_iOS
+//
+//  Created by 곽다은 on 7/11/24.
+//
+
+import UIKit
+
+import SnapKit
+import ReactorKit
+import RxCocoa
+import RxSwift
+
+class HBTIViewController: UIViewController, View {
+    
+    // MARK: - Properties
+    
+    var disposeBag = DisposeBag()
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Bind
+    
+    func bind(reactor: HBTIReactor) {
+        
+        // MARK: Action
+        
+        
+        // MARK: State
+        
+    }
+}
