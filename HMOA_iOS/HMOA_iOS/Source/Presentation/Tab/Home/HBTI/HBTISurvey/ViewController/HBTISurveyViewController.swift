e4b6379a57c61b8e3479035727e371b77b0a39e2
KwakDADA <down022@gmail.com>
2024-07-13 20:03:24 +0900

    [Feat] HBTISurveyVC 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTISurvey/ViewController/HBTISurveyViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HBTISurveyViewController.swift
+//  HMOA_iOS
+//
+//  Created by 곽다은 on 7/12/24.
+//
+
+import UIKit
+
+import SnapKit
+import ReactorKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+class HBTISurveyViewController: UIViewController, View {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Properties
+    
+    var disposeBag = DisposeBag()
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Bind
+    
+    func bind(reactor: HBTISurveyReactor) {
+        
+        // MARK: Action
+        
+        
+        // MARK: State
+        
+    }
+    
+    // MARK: - Functions
+}
