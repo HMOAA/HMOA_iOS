e419c8da5a9f2fae748071ed5215388bb6690346
KwakDADA <down022@gmail.com>
2024-07-13 20:03:24 +0900

    [Feat] HBTISurveyVC 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTISurvey/Reactor/HBTISurveyReactor.swift
@@ -0,0 +1,38 @@
+//
+//  HBTISurveyReactor.swift
+//  HMOA_iOS
+//
+//  Created by 곽다은 on 7/12/24.
+//
+
+import ReactorKit
+import RxSwift
+
+class HBTISurveyReactor: Reactor {
+    
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    var initialState: State
+    
+    init() {
+        self.initialState = State()
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        
+    }
+}
