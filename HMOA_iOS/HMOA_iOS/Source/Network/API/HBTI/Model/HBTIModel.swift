e069d700cb1d525e3c399d3085ccb92564c759fb
KwakDADA <down022@gmail.com>
2024-07-14 19:25:41 +0900

    [Feat] HBTISurvey Section, Item 정의

---
+++ HMOA_iOS/HMOA_iOS/Source/Network/API/HBTI/Model/HBTIModel.swift
@@ -0,0 +1,16 @@
+//
+//  HBTIModel.swift
+//  HMOA_iOS
+//
+//  Created by 곽다은 on 7/14/24.
+//
+
+import Foundation
+
+struct HBTIQuestion: Hashable {
+    
+}
+
+struct HBTIAnswer: Hashable {
+    
+}
