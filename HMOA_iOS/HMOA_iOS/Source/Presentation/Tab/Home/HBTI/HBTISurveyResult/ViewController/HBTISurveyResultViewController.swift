843f2ba79b79fccbc9122fa33da660d1a906abe6
KwakDADA <down022@gmail.com>
2024-08-12 16:17:17 +0900

    [Feat] 향BTI 설문결과VC 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTISurveyResult/ViewController/HBTISurveyResultViewController.swift
@@ -0,0 +1,63 @@
+//
+//  HBTISurveyResultViewController.swift
+//  HMOA_iOS
+//
+//  Created by 곽다은 on 8/12/24.
+//
+
+import UIKit
+
+import SnapKit
+import ReactorKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+class HBTISurveyResultViewController: UIViewController, View {
+    
+    // MARK: - UI Components
+    
+    
+    
+    // MARK: - Properties
+    
+    var disposeBag = DisposeBag()
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setAddView()
+        setConstraints()
+    }
+    
+    // MARK: - Bind
+    
+    func bind(reactor: HBTISurveyResultReactor) {
+        
+        // MARK: Action
+        
+        
+        // MARK: State
+        
+    }
+    
+    // MARK: - Functions
+    
+    private func setUI() {
+        setBackItemNaviBar("향BTI")
+    }
+    
+    // MARK: Add Views
+    private func setAddView() {
+        
+    }
+    
+    // MARK: Set Constraints
+    private func setConstraints() {
+        
+    }
+}
