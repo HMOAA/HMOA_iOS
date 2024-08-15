9b6aa68acef557c8b6a998c1e27a76d617b2bcc9
KwakDADA <down022@gmail.com>
2024-08-15 10:51:15 +0900

    [Feat] HBTI 향료 입력하기 버튼 탭 시 이동할 HBTINoteVC 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTINote/ViewController/HBTINoteViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HBTINoteViewController.swift
+//  HMOA_iOS
+//
+//  Created by 곽다은 on 8/15/24.
+//
+
+import UIKit
+
+import SnapKit
+import ReactorKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+class HBTINoteViewController: UIViewController, View {
+    
+    // MARK: - Properties
+    
+    var disposeBag = DisposeBag()
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func bind(reactor: HBTINoteReactor) {
+        
+        // MARK: Action
+        
+        
+        // MARK: State
+        
+    }
+
+}
