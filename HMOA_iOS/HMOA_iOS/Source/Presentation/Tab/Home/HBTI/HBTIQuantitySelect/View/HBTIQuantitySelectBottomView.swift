e639a308f2f5ec018a1d536f5fe6d3d5e10aaca0
DIN-STUDIO <kht1790@gmail.com>
2024-08-17 01:42:53 +0900

    [Feat] QuantitySelectBottomView 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIQuantitySelect/View/HBTIQuantitySelectBottomView.swift
@@ -0,0 +1,46 @@
+//
+//  HBTIQuantitySelectBottomView.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/17/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class HBTIQuantitySelectBottomView: UIView {
+    
+    // MARK: - UI Components
+    
+    // MARK: - LifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setAddView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Set UI
+    
+    private func setUI() {
+        
+    }
+    
+    // MARK: Add Views
+    
+    private func setAddView() {
+
+    }
+    
+    // MARK: Set Constraints
+    
+    private func setConstraints() {
+        
+    }
+}
