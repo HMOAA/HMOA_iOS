cd0dd81f9fbf5d6338b3bd29f4e317d5d6c998b0
DIN-STUDIO <kht1790@gmail.com>
2024-08-17 01:14:39 +0900

    [Feat] HBTIQuantitySelectTopView 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIQuantitySelect/View/HBTIQuantitySelectTopView.swift
@@ -0,0 +1,44 @@
+//
+//  HBTIQuantitySelectView.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/17/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class HBTIQuantitySelectTopView: UIView {
+    
+    // MARK: - UI Components
+    
+    // MARK: - LifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Set UI
+    
+    private func setUI() {
+        
+    }
+    
+    // MARK: Add Views
+    
+    private func setAddView() {
+        
+    }
+    
+    // MARK: Set Constraints
+    
+    private func setConstraints() {
+        
+    }
+}
