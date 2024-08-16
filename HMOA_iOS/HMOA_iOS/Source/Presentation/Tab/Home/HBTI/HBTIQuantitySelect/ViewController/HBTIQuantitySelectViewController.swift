d7aa226e5eb2a017ee05b456b2b3633bf6ca07b5
DIN-STUDIO <kht1790@gmail.com>
2024-08-16 23:28:03 +0900

    [Feat] QuantityVC 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIQuantitySelect/ViewController/HBTIQuantitySelectViewController.swift
@@ -0,0 +1,56 @@
+//
+//  HBTIQuantitySelectViewController.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/16/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import ReactorKit
+
+final class HBTIQuantitySelectViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setAddView()
+        setConstraints()
+    }
+    
+    // MARK: - Bind
+    
+    func bind(reactor: HBTISurveyReactor) {
+        
+        // MARK: Action
+        
+        
+        // MARK: State
+        
+    }
+    
+    // MARK: Set UI
+    
+    private func setUI() {
+        setBackItemNaviBar("향BTI")
+    }
+    
+    // MARK: Add Views
+    
+    private func setAddView() {
+        
+    }
+    
+    // MARK: Set Constraints
+    
+    private func setConstraints() {
+        
+    }
+}
