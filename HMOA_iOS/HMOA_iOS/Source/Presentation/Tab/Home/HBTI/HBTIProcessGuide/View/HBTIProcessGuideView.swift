986be7227825e9ae97a5b9ab67707ca6f28a76b6
DIN-STUDIO <kht1790@gmail.com>
2024-08-16 18:56:20 +0900

    [Feat] HBTIProcessGuideView 생성

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIProcessGuide/View/HBTIProcessGuideView.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIProcessGuide/View/HBTIProcessGuideView.swift
@@ -5,4 +5,43 @@
 //  Created by HyoTaek on 8/16/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class HBTIProcessGuideView: UIView {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setAddView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Set UI
+    
+    private func setUI() {
+        
+    }
+    
+    // MARK: - Set AddView
+    
+    private func setAddView() {
+        
+    }
+    
+    // MARK: - Set Constraints
+    
+    private func setConstraints() {
+       
+    }
+}
