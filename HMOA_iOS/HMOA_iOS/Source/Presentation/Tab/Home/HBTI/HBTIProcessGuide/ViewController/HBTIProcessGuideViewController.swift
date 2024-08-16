5d8c066661aeeda32e56920d501c72ce1dcf1180
DIN-STUDIO <kht1790@gmail.com>
2024-08-16 16:14:32 +0900

    [Feat] ProcessVC 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIProcessGuide/ViewController/HBTIProcessGuideViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HBTIProcessGuideViewController.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/16/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import ReactorKit
+
+final class HBTIProcessGuideViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+    }
+    
+    private func setUI() {
+        setClearBackNaviBar("향BTI", .black)
+    }
+    
+    func bind() {
+        
+        // MARK: Action
+        
+        // MARK: State
+        
+    }
+}
