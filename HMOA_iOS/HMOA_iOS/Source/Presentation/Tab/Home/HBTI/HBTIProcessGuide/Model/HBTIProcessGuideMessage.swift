779209aa1d314566951cbb507c2172f28e7e1b75
DIN-STUDIO <kht1790@gmail.com>
2024-08-16 19:17:30 +0900

    [Refactor] model의 static 키워드 제거

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIProcessGuide/Model/HBTIProcessGuideMessage.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIProcessGuide/Model/HBTIProcessGuideMessage.swift
@@ -13,10 +13,8 @@ struct HBTIProcessGuideData {
     let description: String
 }
 
-extension HBTIProcessGuideData {
-    let processData = [
-        HBTIProcessGuideData(index: 1, title: "향료 선택", description: "향BTI 검사 이후 추천하는 향료, 원하는 향료 선택\n(기격대 상이)"),
-        HBTIProcessGuideData(index: 2, title: "배송", description: "결제 후 1-2일 내 배송 완료"),
-        HBTIProcessGuideData(index: 3, title: "향수 추천", description: "시향 후 가장 좋았던 향료 선택, 향수 추천 받기")
-    ]
-}
+let processData: [HBTIProcessGuideData] = [
+    HBTIProcessGuideData(index: 1, title: "향료 선택", description: "향BTI 검사 이후 추천하는 향료, 원하는 향료 선택\n(기격대 상이)"),
+    HBTIProcessGuideData(index: 2, title: "배송", description: "결제 후 1-2일 내 배송 완료"),
+    HBTIProcessGuideData(index: 3, title: "향수 추천", description: "시향 후 가장 좋았던 향료 선택, 향수 추천 받기")
+]
