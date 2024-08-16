ed1c76cc9159eec7c8cac4f7a698e3457c3ec219
DIN-STUDIO <kht1790@gmail.com>
2024-08-17 01:23:21 +0900

    [UI] categoryTitleLabel 생성

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIQuantitySelect/View/HBTIQuantitySelectTopView.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIQuantitySelect/View/HBTIQuantitySelectTopView.swift
@@ -13,11 +13,21 @@ final class HBTIQuantitySelectTopView: UIView {
     
     // MARK: - UI Components
     
+    private let categoryTitleLabel = UILabel().then {
+        $0.text = HBTIQuantitySelectionData().titleLabelText
+        $0.textColor = .black
+        $0.font = UIFont.customFont(.pretendard_bold, 20)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
     // MARK: - LifeCycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        setAddView()
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -33,12 +43,17 @@ final class HBTIQuantitySelectTopView: UIView {
     // MARK: Add Views
     
     private func setAddView() {
-        
+        [
+         categoryTitleLabel
+        ].forEach(self.addSubview)
     }
     
     // MARK: Set Constraints
     
     private func setConstraints() {
-        
+        categoryTitleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(127)
+            $0.leading.equalToSuperview().offset(16)
+        }
     }
 }
