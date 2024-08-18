38c2d19e3c1e715cc4a764a8a096b1a08a738906
DIN-STUDIO <kht1790@gmail.com>
2024-08-19 03:19:42 +0900

    [Refactor] UIView에서 UICollectionViewCell로 설계 변경

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Home/ViewController/HomeViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Home/ViewController/HomeViewController.swift
@@ -48,7 +48,8 @@ class HomeViewController: UIViewController, View {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        presentHBTIQuantitySelectViewController()
+//        presentHBTIQuantitySelectViewController()
+        presentHBTISurveyViewController()
         configureUI()
         setSearchBellNaviBar("H  M  O  A", bellButton: bellBarButton)
         configureCollectionViewDataSource()
