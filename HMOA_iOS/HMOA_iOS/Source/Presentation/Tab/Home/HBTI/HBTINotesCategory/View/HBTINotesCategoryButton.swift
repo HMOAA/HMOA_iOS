c008d08c0492f7254c7098d12889a2c8879f7422
DIN-STUDIO <kht1790@gmail.com>
2024-08-19 15:54:58 +0900

    [UI] 버튼 label, image, description 구현

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTINotesCategory/View/HBTINotesCategoryButton.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTINotesCategory/View/HBTINotesCategoryButton.swift
@@ -11,6 +11,25 @@ final class HBTINotesCategoryButton: UIButton {
     
     // MARK: - UI Components
     
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 34
+    }
+    
+    private let titleLabel = UILabel().then {
+        $0.font = .customFont(.pretendard_semibold, 14)
+        $0.textColor = .black
+        $0.textAlignment = .center
+    }
+    
+    private let descriptionLabel = UILabel().then {
+        $0.font = .customFont(.pretendard, 10)
+        $0.textColor = .black
+        $0.textAlignment = .center
+        $0.numberOfLines = 3
+    }
+    
     // MARK: - Init
         
     override init(frame: CGRect) {
@@ -34,13 +53,31 @@ final class HBTINotesCategoryButton: UIButton {
     // MARK: Add Views
     
     private func setAddView() {
-        
+        [
+         imageView,
+         titleLabel,
+         descriptionLabel
+        ].forEach(addSubview)
     }
     
     // MARK: Set Constraints
     
     private func setConstraints() {
+        imageView.snp.makeConstraints {
+            $0.top.centerX.equalToSuperview()
+            $0.size.equalTo(68)
+        }
         
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(12)
+            $0.centerX.equalTo(imageView)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4)
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.width.equalTo(60)
+        }
     }
     
     // MARK: - Configuration
