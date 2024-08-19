7f4692e9eb269a6886943b05ffe68ac585114326
DIN-STUDIO <kht1790@gmail.com>
2024-08-19 09:23:30 +0900

    [Feat] HBTINotesCategoryVC 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTINotesCategory/ViewController/HBTINotesCategoryViewController.swift
@@ -0,0 +1,64 @@
+//
+//  HBTINotesCategoryViewController.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/19/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import ReactorKit
+
+final class HBTINotesCategoryViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setAddView()
+        setConstraints()
+    }
+    
+    // MARK: - Bind
+    
+    func bind(reactor: HBTISurveyReactor) {
+        
+        // MARK: Action
+        
+        
+        // MARK: State
+        
+    }
+    
+    // MARK: Set UI
+    
+    private func setUI() {
+        setBackItemNaviBar("향BTI")
+    }
+    
+    // MARK: Add Views
+    
+    private func setAddView() {
+        
+    }
+    
+    // MARK: Set Constraints
+    
+    private func setConstraints() {
+        
+    }
+    
+    // MARK: Create Layout
+    
+    
+    // MARK: Configure DataSource
+    
+}
