d10d9ca2a78c355c8b4ed4648586c6980c3fe852
DIN-STUDIO <kht1790@gmail.com>
2024-08-19 15:52:36 +0900

    [Feat] 향료 버튼 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTINotesCategory/View/HBTINotesCategoryButton.swift
@@ -0,0 +1,51 @@
+//
+//  HBTINotesCategoryButton.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/19/24.
+//
+
+import Foundation
+
+final class HBTINotesCategoryButton: UIButton {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Init
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setAddView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Set UI
+        
+    private func setUI() {
+
+    }
+    
+    // MARK: Add Views
+    
+    private func setAddView() {
+        
+    }
+    
+    // MARK: Set Constraints
+    
+    private func setConstraints() {
+        
+    }
+    
+    // MARK: - Configuration
+    
+    func configure() {
+        
+    }
+}
