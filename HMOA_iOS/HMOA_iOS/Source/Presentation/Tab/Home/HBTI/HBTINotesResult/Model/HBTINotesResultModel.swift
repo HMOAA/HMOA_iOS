199364261a11e6995fbbaf7f21d52923e37a37b0
DIN-STUDIO <kht1790@gmail.com>
2024-08-23 13:12:15 +0900

    [Feat] HBTINotesResultModel 정의

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTINotesResult/Model/HBTINotesResultModel.swift
@@ -0,0 +1,16 @@
+//
+//  HBTINotesResultModel.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/23/24.
+//
+
+import Foundation
+
+struct HBTINotesResultModel {
+    let id: Int
+    let title: String
+    let subtitle: String
+    let price: Int
+    let description: String
+}
