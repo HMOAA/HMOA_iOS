417a806368e54e25d4118654d2870d98ac68f4d2
DIN-STUDIO <kht1790@gmail.com>
2024-08-24 16:34:37 +0900

    [Feat] UITableFooterView 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTINotesResult/View/HBTINotesResultFooterView.swift
@@ -0,0 +1,50 @@
+//
+//  HBTINotesResultFooterView.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/24/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class HBTINotesResultFooterView: UITableViewHeaderFooterView, ReuseIdentifying {
+    
+    // MARK: - UI Components
+   
+    private let totalPriceLabel = UILabel().then {
+        $0.font = .customFont(.pretendard, 15)
+        $0.text = "총 금액 : 15,000 원"
+        $0.textColor = .black
+        $0.textAlignment = .right
+    }
+    
+    // MARK: - LifeCycle
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+       
+        setAddView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Add Views
+    
+    private func setAddView() {
+        contentView.addSubview(totalPriceLabel)
+    }
+    
+    // MARK: Set Constraints
+    
+    private func setConstraints() {
+        totalPriceLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
