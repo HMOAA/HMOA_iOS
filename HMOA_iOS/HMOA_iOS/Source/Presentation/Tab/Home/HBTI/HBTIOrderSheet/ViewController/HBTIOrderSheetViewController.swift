b923bba433b81f92e885dd558ef5d0ce86c00008
DIN-STUDIO <kht1790@gmail.com>
2024-08-25 17:21:42 +0900

    [Feat] hbtiOrderVC 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/ViewController/HBTIOrderSheetViewController.swift
@@ -0,0 +1,67 @@
+//
+//  HBTIOrderSheetViewController.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/25/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class HBTIOrderSheetViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private let payButton = UIButton().then {
+        $0.setTitle("결제하기", for: .normal)
+        $0.titleLabel?.font = .customFont(.pretendard, 15)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 5
+        $0.backgroundColor = .black
+    }
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setAddView()
+        setConstraints()
+    }
+    
+    // MARK: - Bind
+    
+    func bind() {
+        
+        // MARK: Action
+        
+        // MARK: State
+        
+    }
+    
+    // MARK: - Functions
+    
+    private func setUI() {
+        setBackItemNaviBar("주문서 작성")
+    }
+    
+    // MARK: Add Views
+    
+    private func setAddView() {
+        [
+         payButton
+        ].forEach(view.addSubview)
+    }
+    
+    // MARK: Set Constraints
+    
+    private func setConstraints() {
+        payButton.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().inset(40)
+            $0.height.equalTo(52)
+        }
+    }
+}
