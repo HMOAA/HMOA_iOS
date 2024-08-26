e29de4f44fdef8e8a8bd83f7e652accd49ecc3ae
DIN-STUDIO <kht1790@gmail.com>
2024-08-26 23:50:57 +0900

    [Feat] OrderSheetView 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTIOrderSheetView.swift
@@ -0,0 +1,39 @@
+//
+//  HBTIOrderSheetView.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/26/24.
+//
+
+import UIKit
+
+final class HBTIOrderSheetView: UIView {
+    
+    // MARK: - Initialization
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setAddView()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Set UI
+        
+    private func setUI() {
+
+    }
+    
+    private func setAddView() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
