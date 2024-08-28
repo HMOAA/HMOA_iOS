19f675dbe09fd21b9f2e3bb4e9057640d3d9e2d9
DIN-STUDIO <kht1790@gmail.com>
2024-08-29 00:51:01 +0900

    [UI] AddressView UI 작업 완료

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTIAddressView.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTIAddressView.swift
@@ -6,10 +6,29 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 final class HBTIAddressView: UIView {
     
     // MARK: - UI Components
+    
+    private let titleLabel = UILabel().then {
+        $0.setLabelUI("배송지", font: .pretendard_bold, size: 18, color: .black)
+    }
+    
+    private let saveDeliveryInfoButton = UIButton().then {
+        let text = "배송지를 입력해주세요"
+        let attributedString = NSAttributedString(
+            string: text,
+            attributes: [
+                .font: UIFont.customFont(.pretendard_medium, 10),
+                .foregroundColor: UIColor.black,
+                .underlineStyle: NSUnderlineStyle.single.rawValue
+            ]
+        )
+        $0.setAttributedTitle(attributedString, for: .normal)
+    }
     
     // MARK: - Initialization
     
@@ -34,12 +53,23 @@ final class HBTIAddressView: UIView {
     // MARK: - Set AddView
     
     private func setAddView() {
-        
+        [
+         titleLabel,
+         saveDeliveryInfoButton
+        ].forEach(addSubview)
     }
     
     // MARK: - Set Constraints
     
     private func setConstraints() {
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
         
+        saveDeliveryInfoButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
     }
 }
