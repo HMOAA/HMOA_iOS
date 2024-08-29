765b769a0f61dc78b685bf011be9f86a8fc089e4
DIN-STUDIO <kht1790@gmail.com>
2024-08-29 14:40:39 +0900

    [Refactor] HBTIOrderSheetProductData로 이름 변경

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/Model/HBTIOrderSheetProductData.swift
@@ -0,0 +1,23 @@
+//
+//  HBTIOrderSheetData.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/29/24.
+//
+
+import Foundation
+
+struct HBTIOrderSheetProductData {
+    let image: String
+    let title: String
+    let details: String
+    let count: Int
+    let price: Int
+}
+
+extension HBTIOrderSheetProductData {
+    static let productData: [HBTIOrderSheetProductData] = [
+        HBTIOrderSheetProductData(image: "Citrus", title: "시트러스", details: "라임 만다린, 베르가못, 비터 오렌지, 자몽", count: 4, price: 4800),
+        HBTIOrderSheetProductData(image: "Floral", title: "플로럴", details: "네롤리, 화이트 로즈, 핑크 로즈, 화이트로즈, 바이올렛, 피오니", count: 6, price: 7200)
+    ]
+}
