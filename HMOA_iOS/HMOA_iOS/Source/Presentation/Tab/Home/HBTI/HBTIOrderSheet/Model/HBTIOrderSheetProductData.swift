6b4d625cfe430fd5c7a35d6b07305406fc1656d6
DIN-STUDIO <kht1790@gmail.com>
2024-08-29 15:20:02 +0900

    [Refactor] 주문서 model에 개당 가격 추가

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/Model/HBTIOrderSheetProductData.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/Model/HBTIOrderSheetProductData.swift
@@ -12,12 +12,13 @@ struct HBTIOrderSheetProductData {
     let title: String
     let details: String
     let count: Int
+    let pricePerUnit: String
     let price: Int
 }
 
 extension HBTIOrderSheetProductData {
     static let productData: [HBTIOrderSheetProductData] = [
-        HBTIOrderSheetProductData(image: "Citrus", title: "시트러스", details: "라임 만다린, 베르가못, 비터 오렌지, 자몽", count: 4, price: 4800),
-        HBTIOrderSheetProductData(image: "Floral", title: "플로럴", details: "네롤리, 화이트 로즈, 핑크 로즈, 화이트로즈, 바이올렛, 피오니", count: 6, price: 7200)
+        HBTIOrderSheetProductData(image: "Citrus", title: "시트러스", details: "라임 만다린, 베르가못, 비터 오렌지, 자몽", count: 4, pricePerUnit: "990원/개", price: 4800),
+        HBTIOrderSheetProductData(image: "Floral", title: "플로럴", details: "네롤리, 화이트 로즈, 핑크 로즈, 화이트로즈, 바이올렛, 피오니", count: 6, pricePerUnit: "990원/개", price: 7200)
     ]
 }
