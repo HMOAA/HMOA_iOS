4ba9af0cb5aa7655caec90b61d278a52ae3ee404
DIN-STUDIO <kht1790@gmail.com>
2024-08-29 16:35:21 +0900

    [Feat] productTableView에 cell 등록 및 tableView 설정

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTIProductInfoView.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTIProductInfoView.swift
@@ -11,13 +11,21 @@ import Then
 
 final class HBTIProductInfoView: UIView {
     
+    // MARK: - Properties
+    
+    private let products: [HBTIOrderSheetProductData] = HBTIOrderSheetProductData.productData
+    
     // MARK: - UI Components
     
     private let titleLabel = UILabel().then {
         $0.setLabelUI("상품 정보", font: .pretendard_bold, size: 18, color: .black)
     }
     
-    private let productTableView = UITableView()
+    private lazy var productTableView = UITableView().then {
+        $0.register(HBTIProductInfoCell.self, forCellReuseIdentifier: HBTIProductInfoCell.reuseIdentifier)
+        $0.dataSource = self
+        $0.delegate = self
+    }
     
     // MARK: - Initialization
 
@@ -49,5 +57,22 @@ final class HBTIProductInfoView: UIView {
 
     private func setConstraints() {
         
+    }
+}
+
+extension HBTIProductInfoView: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HBTIProductInfoCell.reuseIdentifier, for: indexPath) as? HBTIProductInfoCell else {
+            return UITableViewCell()
+        }
+        
+        let product = products[indexPath.row]
+        cell.configureCell(with: product)
+        
+        return cell
     }
 }
