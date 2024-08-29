c75dc850c04e069e055b3f480c1a38adb8e40b10
DIN-STUDIO <kht1790@gmail.com>
2024-08-29 15:17:52 +0900

    [UI] 상품 정보 cell UI 작업

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTIProductInfoCell.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTIProductInfoCell.swift
@@ -13,6 +13,37 @@ final class HBTIProductInfoCell: UITableViewCell {
     
     // MARK: - UI Components
     
+    private let productImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.layer.cornerRadius = 30
+        $0.layer.masksToBounds = true
+    }
+    
+    private let productTitleLabel = UILabel().then {
+        $0.setLabelUI("상품명", font: .pretendard_semibold, size: 14, color: .black)
+    }
+    
+    private let productDescriptionLabel = UILabel().then {
+        $0.setLabelUI("상품 상세 설명", font: .pretendard, size: 10, color: .black)
+        $0.numberOfLines = 0
+    }
+   
+    private let productCountLabel = UILabel().then {
+        $0.setLabelUI("수량 0개", font: .pretendard, size: 10, color: .gray3)
+    }
+    
+    private let removeProductButton = UIButton().then {
+        $0.setImage(UIImage(named: "x"), for: .normal)
+    }
+   
+    private let productPricePerUnitLabel = UILabel().then {
+        $0.setLabelUI("0원/개", font: .pretendard, size: 10, color: .gray3)
+    }
+    
+    private let productPriceLabel = UILabel().then {
+        $0.setLabelUI("0원", font: .pretendard_semibold, size: 14, color: .black)
+    }
+    
     // MARK: - Initialization
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
