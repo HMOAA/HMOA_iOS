e8562cf5628a7e47b1343e941011e29343e23577
DIN-STUDIO <kht1790@gmail.com>
2024-08-30 06:05:11 +0900

    [Feat] 총 결제금액 view 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTITotalPaymentView.swift
@@ -0,0 +1,47 @@
+//
+//  HBTITotalPaymentView.swift
+//  HMOA_iOS
+//
+//  Created by HyoTaek on 8/30/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class HBTITotalPaymentView: UIView {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Initialization
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setAddView()
+        setConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Set UI
+
+    private func setUI() {
+        
+    }
+
+    // MARK: - Set AddView
+
+    private func setAddView() {
+        
+    }
+
+    // MARK: - Set Constraints
+
+    private func setConstraints() {
+        
+    }
+}
