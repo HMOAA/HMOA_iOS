698acd5adf87b623e829d41d48074afe9768f118
DIN-STUDIO <kht1790@gmail.com>
2024-08-30 06:10:50 +0900

    [Feat] 결제 금액 label 생성

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTITotalPaymentView.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTITotalPaymentView.swift
@@ -13,6 +13,15 @@ final class HBTITotalPaymentView: UIView {
     
     // MARK: - UI Components
     
+    private let paymentTitleLabel = UILabel().then {
+        $0.setLabelUI("결제금액", font: .pretendard_bold, size: 18, color: .black)
+    }
+    
+    private let paymentLabel = UILabel().then {
+        $0.setLabelUI("15,000원", font: .pretendard_bold, size: 18, color: .red)
+    }
+    
+    private
     // MARK: - Initialization
 
     override init(frame: CGRect) {
