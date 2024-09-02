81c0bd354364b5a4df4c52761245ffed36c426ea
DIN-STUDIO <kht1790@gmail.com>
2024-09-02 22:20:25 +0900

    [UI] 주소 추가VC의 주소textFieldView의 컴포넌트들 생성

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTIAddressTextFieldView.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/View/HBTIAddressTextFieldView.swift
@@ -13,6 +13,29 @@ final class HBTIAddressTextFieldView: UIView {
     
     // MARK: UI Components
     
+    private let postCodeTextField = UITextField().then {
+        $0.setTextFieldUI("우편번호", leftPadding: 12, font: .pretendard_medium, isCapsule: true)
+        $0.layer.cornerRadius = 5
+        $0.layer.masksToBounds = true
+    }
+    
+    private let findAddressButton = UIButton().then {
+        $0.setTitle("주소 찾기", for: .normal)
+        $0.backgroundColor = .customColor(.gray1)
+    }
+    
+    private let addressTextField = UITextField().then {
+        $0.setTextFieldUI("주소", leftPadding: 12, font: .pretendard_medium, isCapsule: true)
+        $0.layer.cornerRadius = 5
+        $0.layer.masksToBounds = true
+    }
+    
+    private let detailAddressTextField = UITextField().then {
+        $0.setTextFieldUI("상세주소", leftPadding: 12, font: .pretendard_medium, isCapsule: true)
+        $0.layer.cornerRadius = 5
+        $0.layer.masksToBounds = true
+    }
+    
     // MARK: - Initialization
 
     override init(frame: CGRect) {
@@ -30,7 +53,10 @@ final class HBTIAddressTextFieldView: UIView {
 
     private func setAddView() {
         [
-         
+         postCodeTextField,
+         findAddressButton,
+         addressTextField,
+         detailAddressTextField
         ].forEach(addSubview)
     }
 
