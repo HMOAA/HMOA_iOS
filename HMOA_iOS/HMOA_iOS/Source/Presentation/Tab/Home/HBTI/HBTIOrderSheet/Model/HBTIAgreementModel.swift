371491ece937218f60e57bc3ec9734ec8f8247e6
DIN-STUDIO <kht1790@gmail.com>
2024-09-04 22:43:11 +0900

    [Refactor] 주문동의model의 모두 동의 타입 속성 추가

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/Model/HBTIAgreementModel.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIOrderSheet/Model/HBTIAgreementModel.swift
@@ -12,7 +12,9 @@ struct HBTIAgreementModel {
 }
 
 extension HBTIAgreementModel {
-    static let agreementData: [HBTIAgreementModel] = [
+    static let allAgreementData: HBTIAgreementModel = HBTIAgreementModel(agreementTitle: "주문 내용을 확인했으며, 아래의 정보 제공 및 결제에 모두 동의합니다.")
+    
+    static let partialAgreementData: [HBTIAgreementModel] = [
         HBTIAgreementModel(agreementTitle: "배송/취소/반품/교환정책"),
         HBTIAgreementModel(agreementTitle: "개인정보 제공 동의")
     ]
