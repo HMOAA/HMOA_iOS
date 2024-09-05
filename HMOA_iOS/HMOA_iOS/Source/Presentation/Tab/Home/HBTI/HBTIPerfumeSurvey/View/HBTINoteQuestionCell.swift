d1a48084972a05b320c919733c92193bef57cec9
KwakDADA <down022@gmail.com>
2024-09-05 20:09:30 +0900

    [UI] 향료선택 Cell 내부에 CollectionView 추가 및 레이아웃 정의

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIPerfumeSurvey/View/HBTINoteQuestionCell.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIPerfumeSurvey/View/HBTINoteQuestionCell.swift
@@ -21,6 +21,11 @@ class HBTINoteQuestionCell: UICollectionViewCell {
         $0.setTextWithLineHeight(text: "선택안내", lineHeight: 27)
     }
     
+    private lazy var noteCategoryCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: createLayout()
+    )
+    
     
     // MARK: - Init
     
@@ -44,7 +49,34 @@ class HBTINoteQuestionCell: UICollectionViewCell {
     }
     
     private func setConstraints() {
+        
     }
+    
+    // MARK: Create Layout
+    private func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            )
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            )
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = 24
+            
+            return section
+        }
+        return layout
+    }
+    
     
     func configureCell(question: HBTINoteQuestion) {
         selectLabel.text = question.content
