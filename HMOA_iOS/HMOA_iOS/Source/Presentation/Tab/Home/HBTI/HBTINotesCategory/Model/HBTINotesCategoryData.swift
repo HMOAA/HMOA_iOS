090fcf8444adf939327dca6588deb7b77d90b849
DIN-STUDIO <kht1790@gmail.com>
2024-09-20 20:42:16 +0900

    [Refactor] 함수 이름 변경, 문구 수정

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTINotesCategory/Model/HBTINotesCategoryData.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTINotesCategory/Model/HBTINotesCategoryData.swift
@@ -29,7 +29,7 @@ extension HBTINotesCategoryData {
 struct HBTICategoryLabelTexts {
     let titleLabelText = """
                          추천받은 카테고리는 '시트러스' 입니다.
-                         원하는 카테고리 배송 수량을
+                         원하는 시향 카드 배송 수량을
                          선택해주세요
                          """
     let descriptionLabelText = "· 향료 1개 당 990원"
