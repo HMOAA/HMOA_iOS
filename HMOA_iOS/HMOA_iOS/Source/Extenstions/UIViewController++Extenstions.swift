d3218424a04fd53c4bc1719ad53d7694966ea1ba
DIN-STUDIO <kht1790@gmail.com>
2024-09-20 20:42:16 +0900

    [Refactor] 함수 이름 변경, 문구 수정

--- HMOA_iOS/HMOA_iOS/Source/Extenstions/UIViewController++Extenstions.swift
+++ HMOA_iOS/HMOA_iOS/Source/Extenstions/UIViewController++Extenstions.swift
@@ -321,10 +321,10 @@ extension UIViewController {
         self.navigationController?.pushViewController(hbtiOrderSheetVC, animated: true)
     }
     
-    func presentHBTIAddAddressViewController() {
-        let hbtiAddAddressVC = HBTIAddFixAddressViewController()
-        hbtiAddAddressVC.hidesBottomBarWhenPushed = true
-        self.navigationController?.pushViewController(hbtiAddAddressVC, animated: true)
+    func presentHBTIAddFixAddressViewController() {
+        let hbtiAddFixAddressVC = HBTIAddFixAddressViewController()
+        hbtiAddFixAddressVC.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(hbtiAddFixAddressVC, animated: true)
     }
     
     /// HBTINotesResultVC로 push
