ed138b00f7bca8fec6331443629bdfad960835d3
KwakDADA <down022@gmail.com>
2024-09-26 14:02:14 +0900

    [UI] 취소/환불내역 테이블뷰 데이터소스 설정

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/OrderCancelLog/ViewController/OrderCancelLogViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/OrderCancelLog/ViewController/OrderCancelLogViewController.swift
@@ -16,9 +16,13 @@ import Then
 final class OrderCancelLogViewController: UIViewController, View {
 
     // MARK: - UI Components
-
+    private lazy var orderCancleLogTableView = UITableView(frame: .zero, style: .plain).then {
+        $0.register(OrderCancelLogCell.self, forCellReuseIdentifier: OrderCancelLogCell.identifier)
+    }
 
     // MARK: - Properties
+    
+    private var dataSource: UITableViewDiffableDataSource<OrderCancelLogSection, OrderCancelLogItem>?
 
     var disposeBag = DisposeBag()
 
@@ -30,6 +34,7 @@ final class OrderCancelLogViewController: UIViewController, View {
         setUI()
         setAddView()
         setConstraints()
+        configureDatasource()
     }
 
     // MARK: - Bind
@@ -46,16 +51,48 @@ final class OrderCancelLogViewController: UIViewController, View {
     private func setUI() {
         setClearBackNaviBar("환불 / 반품 내역", .black)
         view.backgroundColor = .white
+        orderCancleLogTableView.separatorStyle = .none
     }
 
     // MARK: Add Views
     private func setAddView() {
-
+        [
+            orderCancleLogTableView
+        ]   .forEach { view.addSubview($0) }
     }
 
     // MARK: Set Constraints
     private func setConstraints() {
-
+        orderCancleLogTableView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(3)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Data Source Initialization
+    
+    private func configureDatasource() {
+        dataSource = .init(tableView: orderCancleLogTableView,
+                           cellProvider: { (tableView, indexPath, item) -> UITableViewCell? in
+            
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: OrderCancelLogCell.identifier,
+                for: indexPath) as! OrderCancelLogCell
+            
+            cell.configureCell()
+            cell.selectionStyle = .none
+            
+            return cell
+        })
+        
+        // MARK: Initial Snapshot
+        
+        var initialSnapshot = NSDiffableDataSourceSnapshot<OrderCancelLogSection, OrderCancelLogItem>()
+        initialSnapshot.appendSections([.cancel])
+        initialSnapshot.appendItems([.order("1"), .order("2")])
+        
+        dataSource?.apply(initialSnapshot)
     }
 
 }
