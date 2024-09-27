0cca0623fff02e6494a3d638f9d8d4185e0ffe6e
KwakDADA <down022@gmail.com>
2024-09-27 14:02:10 +0900

    [UI] 취소/환불내역 화면 상단 뷰 컴포넌트 추가 및 배치

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/OrderCancelDetail/ViewController/OrderCancelDetailViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/MyPage/OrderCancelDetail/ViewController/OrderCancelDetailViewController.swift
@@ -17,6 +17,27 @@ final class OrderCancelDetailViewController: UIViewController, View {
 
     // MARK: - UI Components
     
+    private let scrollView = UIScrollView()
+    
+    private let categoryStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 16
+        $0.alignment = .fill
+        $0.distribution = .equalSpacing
+    }
+    
+    private let separatorLineView = UIView().then {
+        $0.backgroundColor = .black
+    }
+    
+    private let totalAmountTitleLabel = UILabel().then {
+        $0.setLabelUI("", font: .pretendard_bold, size: 18, color: .black)
+        $0.setTextWithLineHeight(text: "결제금액", lineHeight: 20)
+    }
+    
+    private let totalAmountValueLabel = UILabel().then {
+        $0.setLabelUI("15,000원", font: .pretendard_bold, size: 20, color: .red)
+    }
     
     // MARK: - Properties
     
@@ -50,13 +71,53 @@ final class OrderCancelDetailViewController: UIViewController, View {
     
     // MARK: Add Views
     private func setAddView() {
+        view.addSubview(scrollView)
+        
         [
-            
-        ]   .forEach { view.addSubview($0) }
+            categoryStackView,
+            separatorLineView,
+            totalAmountTitleLabel,
+            totalAmountValueLabel
+        ]   .forEach { scrollView.addSubview($0) }
+        
+        [
+            OrderCancelCategoryView(),
+            OrderCancelCategoryView()
+        ]   .forEach {
+            $0.configureView()
+            $0.snp.makeConstraints { make in
+                make.height.greaterThanOrEqualTo(60)
+            }
+            categoryStackView.addArrangedSubview($0)
+        }
     }
     
     // MARK: Set Constraints
     private func setConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
         
+        categoryStackView.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview().inset(20)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.width.equalTo(scrollView.snp.width).offset(-32)
+        }
+        
+        separatorLineView.snp.makeConstraints { make in
+            make.top.equalTo(categoryStackView.snp.bottom).offset(24)
+            make.horizontalEdges.equalTo(categoryStackView.snp.horizontalEdges)
+            make.height.equalTo(1)
+        }
+        
+        totalAmountTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(separatorLineView.snp.bottom).offset(24)
+            make.leading.equalTo(separatorLineView.snp.leading)
+        }
+        
+        totalAmountValueLabel.snp.makeConstraints { make in
+            make.top.equalTo(totalAmountTitleLabel.snp.top)
+            make.trailing.equalTo(separatorLineView.snp.trailing)
+        }
     }
 }
