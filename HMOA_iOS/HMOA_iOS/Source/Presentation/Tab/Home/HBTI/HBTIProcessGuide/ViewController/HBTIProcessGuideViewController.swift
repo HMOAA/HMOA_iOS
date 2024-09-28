2971e0a7b6c42c4e7fde3fe5262c78bb7cf93f0d
DIN-STUDIO <kht1790@gmail.com>
2024-09-28 18:46:52 +0900

    [Refactor] Reactor 초기화 변경 및 presentHBTIProcess, Quantity, NoteCategory 함수 수정

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIProcessGuide/ViewController/HBTIProcessGuideViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIProcessGuide/ViewController/HBTIProcessGuideViewController.swift
@@ -26,15 +26,6 @@ final class HBTIProcessGuideViewController: UIViewController, View {
     
     // MARK: - LifeCycle
     
-    init(reactor: HBTIProcessGuideReactor) {
-        super.init(nibName: nil, bundle: nil)
-        self.reactor = reactor
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
