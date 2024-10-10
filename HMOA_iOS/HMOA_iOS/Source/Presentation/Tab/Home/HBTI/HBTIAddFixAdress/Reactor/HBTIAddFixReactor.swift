77d40207a4d307aa570f0b024c5e092ad402b386
DIN-STUDIO <kht1790@gmail.com>
2024-10-11 01:25:03 +0900

    [Feat] 주소 추가 및 변경 reactor 생성

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIAddFixAdress/Reactor/HBTIAddFixReactor.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIAddFixAdress/Reactor/HBTIAddFixReactor.swift
@@ -5,4 +5,49 @@
 //  Created by HyoTaek on 10/11/24.
 //
 
-import Foundation
+import RxSwift
+import ReactorKit
+
+final class HBTIAddFixReactor: Reactor {
+    
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        var name: String = ""
+        var addressName: String = ""
+        var phoneNumber: String = ""
+        var telephoneNumber: String = ""
+        var zipCode: String = ""
+        var address: String = ""
+        var detailAddress: String = ""
+        var orderRequest: String = ""
+    }
+    
+    var initialState: State
+    
+    init() {
+        self.initialState = State()
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        
+        switch mutation {
+       
+        }
+        
+        return state
+    }
+}
