1f8e2a9773ccf9fd8d557447ac7f15fe92058928
DIN-STUDIO <kht1790@gmail.com>
2024-10-13 02:42:03 +0900

    [Feat] 주소 추가에서 필수 항목 미입력시 저장하기 버튼 비활성화

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIAddFixAdress/Reactor/HBTIAddFixReactor.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIAddFixAdress/Reactor/HBTIAddFixReactor.swift
@@ -11,11 +11,24 @@ import ReactorKit
 final class HBTIAddFixReactor: Reactor {
     
     enum Action {
-        
+        case didChangeName(String)
+        case didChangeAddressName(String)
+        case didChangePhoneNumber(String)
+        case didChangeTelephoneNumber(String)
+        case didChangeDetailAddress(String)
+        case didChangeOrderRequest(String)
+        case didTapSaveButton
     }
     
     enum Mutation {
-        
+        case setName(String)
+        case setAddressName(String)
+        case setPhoneNumber(String)
+        case setTelephoneNumber(String)
+        case setDetailAddress(String)
+        case setOrderRequest(String)
+        case setIsEnabledSaveButton(Bool)
+        case setIsPushVC(Bool)
     }
     
     struct State {
@@ -24,10 +37,12 @@ final class HBTIAddFixReactor: Reactor {
         var addressName: String = ""
         var phoneNumber: String = ""
         var telephoneNumber: String = ""
-        var zipCode: String = ""
-        var address: String = ""
+        var zipCode: String = "12345"
+        var address: String = "인천 연수구"
         var detailAddress: String = ""
         var orderRequest: String = ""
+        var isEnabledSaveButton: Bool = false
+        var isPushVC: Bool = false
     }
     
     var initialState: State
@@ -38,7 +53,28 @@ final class HBTIAddFixReactor: Reactor {
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
-        
+        case .didChangeName(let name):
+            return .just(.setName(name))
+            
+        case .didChangeAddressName(let addressName):
+            return .just(.setAddressName(addressName))
+            
+        case .didChangePhoneNumber(let phoneNumber):
+            return .just(.setPhoneNumber(phoneNumber))
+            
+        case .didChangeTelephoneNumber(let telephoneNumber):
+            return .just(.setTelephoneNumber(telephoneNumber))
+            
+        case .didChangeDetailAddress(let detailAddress):
+            return .just(.setDetailAddress(detailAddress))
+            
+        case .didChangeOrderRequest(let orderRequest):
+            return .just(.setOrderRequest(orderRequest))
+            
+        case .didTapSaveButton:
+            let isEnabled = currentState.isEnabledSaveButton
+            
+            return .just(.setIsPushVC(isEnabled))
         }
     }
     
@@ -46,8 +82,32 @@ final class HBTIAddFixReactor: Reactor {
         var state = state
         
         switch mutation {
-       
+        case .setName(let name):
+            state.name = name
+            
+        case .setAddressName(let addressName):
+            state.addressName = addressName
+            
+        case .setPhoneNumber(let phoneNumber):
+            state.phoneNumber = phoneNumber
+            
+        case .setTelephoneNumber(let telephoneNumber):
+            state.telephoneNumber = telephoneNumber
+            
+        case .setDetailAddress(let detailAddress):
+            state.detailAddress = detailAddress
+            
+        case .setOrderRequest(let orderRequest):
+            state.orderRequest = orderRequest
+            
+        case .setIsEnabledSaveButton(let isEnabled):
+            state.isEnabledSaveButton = isEnabled
+            
+        case .setIsPushVC(let isPush):
+            state.isPushVC = isPush
         }
+        
+        state.isEnabledSaveButton = !state.name.isEmpty && !state.phoneNumber.isEmpty && !state.telephoneNumber.isEmpty && !state.zipCode.isEmpty && !state.address.isEmpty && !state.detailAddress.isEmpty
         
         return state
     }
