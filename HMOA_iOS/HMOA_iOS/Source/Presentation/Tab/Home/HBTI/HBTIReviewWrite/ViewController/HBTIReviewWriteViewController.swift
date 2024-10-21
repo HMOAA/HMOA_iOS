7b12dbf6fcd040947ed4518896048c375d7b25cb
KwakDADA <down022@gmail.com>
2024-10-21 16:05:23 +0900

    [UI] 네비바 및 배경색 적용

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIReviewWrite/ViewController/HBTIReviewWriteViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIReviewWrite/ViewController/HBTIReviewWriteViewController.swift
@@ -18,6 +18,23 @@ final class HBTIReviewWriteViewController: UIViewController, View {
     
     // MARK: - UI Components
     
+    // Navigation bar items
+    private let okButton = UIButton().then {
+        $0.setTitle("확인", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = .customFont(.pretendard, 16)
+    }
+    
+    private let titleNaviLabel = UILabel().then {
+        $0.setLabelUI("향BTI 후기", font: .pretendard_medium, size: 20, color: .white)
+        $0.font = .customFont(.pretendard_medium, 20)
+    }
+    
+    private let cancleButton = UIButton().then {
+        $0.setTitle("취소", for: .normal)
+        $0.titleLabel?.font = .customFont(.pretendard, 16)
+        $0.setTitleColor(.white, for: .normal)
+    }
     
     // MARK: - Properties
     
@@ -48,7 +65,9 @@ final class HBTIReviewWriteViewController: UIViewController, View {
     
     // MARK: Set UI
     private func setUI() {
-        
+        setOkCancleNavigationBar(okButton: okButton, cancleButton: cancleButton, titleLabel: titleNaviLabel)
+        navigationController?.navigationBar.backgroundColor = .customColor(.gray4)
+        view.backgroundColor = .customColor(.gray4)
     }
     
     // MARK: Add Views
