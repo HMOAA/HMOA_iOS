59c6ad5f37c978f468ec650c6f9e4d3611260208
KwakDADA <down022@gmail.com>
2024-10-22 09:25:24 +0900

    [UI] 리뷰Cell 생성

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/Common/View/HBTIReviewCell.swift
@@ -0,0 +1,60 @@
+//
+//  HBTIReviewCell.swift
+//  HMOA_iOS
+//
+//  Created by 곽다은 on 10/22/24.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+import RxSwift
+
+final class HBTIReviewCell: UICollectionViewCell {
+    
+    static let identifier = "HBTIReviewCell"
+    
+    var disposeBag = DisposeBag()
+    
+    // MARK: - UI Components
+    
+    private let reviewView = HBTIReviewView()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setAddView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+    
+    // MARK: - Function
+    
+    private func setAddView() {
+        [
+            reviewView
+        ].forEach { addSubview($0) }
+    }
+    
+    private func setConstraints() {
+        reviewView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func configureCell() {
+        
+    }
+}
+
