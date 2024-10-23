8aeecb4031fcba56759712f9ee29407117cf3e83
KwakDADA <down022@gmail.com>
2024-10-23 17:43:17 +0900

    [Refactor] 향BTI 홈 컬렉션뷰 설문섹션 추가

---
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTI/Model/HBTIHomeSurvey.swift
@@ -0,0 +1,13 @@
+//
+//  HBTIHomeSurvey.swift
+//  HMOA_iOS
+//
+//  Created by 곽다은 on 10/23/24.
+//
+
+import UIKit
+
+struct HBTIHomeSurvey: Hashable {
+    let title: String
+    let imageName: String
+}
--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTI/ViewController/HBTIViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTI/ViewController/HBTIViewController.swift
@@ -18,22 +18,20 @@ final class HBTIViewController: UIViewController, View {
     
     // MARK: - UI Components
     
-    private let yourHBTIView = HBTIHomeTopView().then {
-        $0.backgroundColor = .clear
-    }
-    
     private lazy var hbtiHomeCollectionView = UICollectionView(
         frame: .zero,
         collectionViewLayout: createLayout()
     ).then {
         $0.backgroundColor = .clear
+        $0.register(HBTIHomeSurveyCell.self, forCellWithReuseIdentifier: HBTIHomeSurveyCell.identifier)
         $0.register(HBTIReviewCell.self, forCellWithReuseIdentifier: HBTIReviewCell.identifier)
         $0.register(HBTIHomeReviewHeaderView.self, forSupplementaryViewOfKind: SupplementaryViewKind.header, withReuseIdentifier: HBTIHomeReviewHeaderView.identifier)
     }
     
     // MARK: - Properties
     
-    private var dataSource: UICollectionViewDiffableDataSource<HBTIReviewListSection, HBTIReviewListItem>?
+    private var sections = [HBTIHomeSection]()
+    private var dataSource: UICollectionViewDiffableDataSource<HBTIHomeSection, HBTIHomeItem>?
     var disposeBag = DisposeBag()
     
     // MARK: - LifeCycle
@@ -61,16 +59,6 @@ final class HBTIViewController: UIViewController, View {
         // MARK: Action
         rx.viewDidLoad
             .map { Reactor.Action.viewDidLoad }
-            .bind(to: reactor.action)
-            .disposed(by: disposeBag)
-        
-        yourHBTIView.goToSurveyButton.rx.tap
-            .map { Reactor.Action.didTapSurveyButton }
-            .bind(to: reactor.action)
-            .disposed(by: disposeBag)
-        
-        yourHBTIView.selectNoteButton.rx.tap
-            .map { Reactor.Action.didTapNoteButton }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
         
@@ -124,27 +112,51 @@ final class HBTIViewController: UIViewController, View {
         let layout = UICollectionViewCompositionalLayout {
             (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
             
-            let headerItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(40))
-            let headerItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerItemSize, elementKind: SupplementaryViewKind.header, alignment: .top)
-            
-            let itemSize = NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(1),
-                heightDimension: .estimated(130)
-            )
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            
-            let groupSize = NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(1),
-                heightDimension: .estimated(130)
-            )
-            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
-            
-            let section = NSCollectionLayoutSection(group: group)
-            section.interGroupSpacing = 12
-            section.boundarySupplementaryItems = [headerItem]
-            section.contentInsets = .init(top: 20, leading: 16, bottom: 20, trailing: 16)
-            
-            return section
+            let sections = self.sections[sectionIndex]
+            switch sections {
+            case .survey:
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(0.5),
+                    heightDimension: .estimated(107)
+                )
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .estimated(107)
+                )
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+                group.interItemSpacing = .fixed(12)
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.interGroupSpacing = 12
+                section.boundarySupplementaryItems = []
+                section.contentInsets = .init(top: 20, leading: 16, bottom: 20, trailing: 16)
+                
+                return section
+            case .review:
+                let headerItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(40))
+                let headerItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerItemSize, elementKind: SupplementaryViewKind.header, alignment: .top)
+                
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .estimated(130)
+                )
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .estimated(130)
+                )
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.interGroupSpacing = 12
+                section.boundarySupplementaryItems = [headerItem]
+                section.contentInsets = .init(top: 20, leading: 16, bottom: 20, trailing: 16)
+                
+                return section
+            }
         }
         return layout
     }
@@ -154,6 +166,15 @@ final class HBTIViewController: UIViewController, View {
         dataSource = .init(collectionView: hbtiHomeCollectionView, cellProvider: { (collectionView, indexPath, item) -> UICollectionViewCell? in
             
             switch item {
+            case .survey(let survey):
+                let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: HBTIHomeSurveyCell.identifier,
+                    for: indexPath) as! HBTIHomeSurveyCell
+                
+                cell.configureCell(survey: survey)
+                
+                return cell
+                
             case .review(let review):
                 let cell = collectionView.dequeueReusableCell(
                     withReuseIdentifier: HBTIReviewCell.identifier,
@@ -183,12 +204,15 @@ final class HBTIViewController: UIViewController, View {
             }
         }
         
-        var initialSnapshot = NSDiffableDataSourceSnapshot<HBTIReviewListSection, HBTIReviewListItem>()
-        initialSnapshot.appendSections([.review])
+        var initialSnapshot = NSDiffableDataSourceSnapshot<HBTIHomeSection, HBTIHomeItem>()
+        initialSnapshot.appendSections([.survey, .review])
+        sections = initialSnapshot.sectionIdentifiers
+        
+        initialSnapshot.appendItems(HBTIHomeItem.surveys, toSection: .survey)
         initialSnapshot.appendItems([
-            HBTIReviewListItem.review(HBTIReview(id: 1, profileImageURL: "", author: "작성자", content: "내용", imageCount: 0, photoList: [], date: "어제", isWrited: false, likeCount: 0, isLiked: false, orderTitle: "시향카드")),
-            HBTIReviewListItem.review(HBTIReview(id: 2, profileImageURL: "", author: "작성자", content: "내용", imageCount: 0, photoList: [], date: "어제", isWrited: false, likeCount: 0, isLiked: false, orderTitle: "시향카드")),
-            HBTIReviewListItem.review(HBTIReview(id: 3, profileImageURL: "", author: "작성자", content: "내용", imageCount: 0, photoList: [], date: "어제", isWrited: false, likeCount: 0, isLiked: false, orderTitle: "시향카드"))
+            .review(HBTIReview(id: 1, profileImageURL: "", author: "작성자", content: "내용", imageCount: 0, photoList: [], date: "어제", isWrited: false, likeCount: 0, isLiked: false, orderTitle: "시향카드")),
+            .review(HBTIReview(id: 2, profileImageURL: "", author: "작성자", content: "내용", imageCount: 0, photoList: [], date: "어제", isWrited: false, likeCount: 0, isLiked: false, orderTitle: "시향카드")),
+            .review(HBTIReview(id: 3, profileImageURL: "", author: "작성자", content: "내용", imageCount: 0, photoList: [], date: "어제", isWrited: false, likeCount: 0, isLiked: false, orderTitle: "시향카드"))
         ], toSection: .review)
         
         dataSource?.apply(initialSnapshot, animatingDifferences: false)
