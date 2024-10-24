1cf3cda8aaec39fbdfa92adc5c0bc63ccecaadd7
KwakDADA <down022@gmail.com>
2024-10-24 14:44:07 +0900

    [Feat] 사진 탭 시 전체보기 기능 추가

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIReviewList/ViewController/HBTIReviewListViewController.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/HBTI/HBTIReviewList/ViewController/HBTIReviewListViewController.swift
@@ -222,6 +222,12 @@ final class HBTIReviewListViewController: UIViewController, View {
                     }
                     .disposed(by: cell.disposeBag)
                 
+                cell.reviewView.photoCollectionView.rx.itemSelected
+                    .bind(with: self, onNext: { owner, indexPath in
+                        owner.presentImageListVC(indexPath, images: item.review!.photoList)
+                    })
+                    .disposed(by: cell.disposeBag)
+                
                 return cell
             }
         })
