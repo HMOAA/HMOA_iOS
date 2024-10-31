cd72f498e85929f10f75db4bfd1e3c0787a17c0b
KwakDADA <down022@gmail.com>
2024-10-31 12:04:46 +0900

    [Refactor] 닉네임중복체크 불필요한 에러처리 제거

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Login/Start/Reactor/NicknameReactor.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Login/Start/Reactor/NicknameReactor.swift
@@ -40,10 +40,7 @@ class NicknameReactor: Reactor {
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
         case .didTapDuplicateButton:
-            guard let nickname = currentState.nickname
-            else { return .just(.setIsDuplicate(true))}
-            
-            if nickname.isEmpty { return .just(.setIsDuplicate(true))}
+            guard let nickname = currentState.nickname else { return .empty() }
             
             return .concat([
                 MemberAPI.checkDuplicateNickname(params: ["nickname": nickname])
@@ -51,8 +48,7 @@ class NicknameReactor: Reactor {
                 .just(.setNickname(nickname))
             ])
         case .didTapStartButton:
-            guard let nickname = currentState.nickname
-            else { return .just(.setIsPushNextVC(false)) }
+            guard currentState.nickname != nil else { return .just(.setIsPushNextVC(false)) }
             return .concat([
                 .just(.setIsPushNextVC(true)),
                 .just(.setIsPushNextVC(false))
