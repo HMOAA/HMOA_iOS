1640062a7480ab985bb8c9cb34a88adba849dcaf
KwakDADA <down022@gmail.com>
2025-01-06 13:21:28 +0900

    [UI] HomeFooterView 정의

--- HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Home/View/HomeFooterView.swift
+++ HMOA_iOS/HMOA_iOS/Source/Presentation/Tab/Home/Home/View/HomeFooterView.swift
@@ -1,5 +1,5 @@
 //
-//  HomeBusinessInfoView.swift
+//  HomeFooterView.swift
 //  HMOA_iOS
 //
 //  Created by 곽다은 on 1/6/25.
@@ -7,6 +7,86 @@
 
 import UIKit
 
-class HomeBusinessInfoView: UICollectionReusableView {
+class HomeFooterView: UICollectionReusableView, ReuseIdentifying {
+    
+    // MARK: - UI Components
+    
+    let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 4
+        $0.distribution = .equalSpacing
+        $0.alignment = .center
+    }
+    
+    let businessNumberLabel = UILabel().then {
+        $0.setLabelUI("", font: .pretendard, size: 10, color: .white)
+    }
+    
+    let representativeLabel = UILabel().then {
+        $0.setLabelUI("", font: .pretendard, size: 10, color: .white)
+    }
+    
+    let privacyOfficerLabel = UILabel().then {
+        $0.setLabelUI("", font: .pretendard, size: 10, color: .white)
+    }
+    
+    let salesLicenseLabel = UILabel().then {
+        $0.setLabelUI("", font: .pretendard, size: 10, color: .white)
+    }
+    
+    let addressLabel = UILabel().then {
+        $0.setLabelUI("", font: .pretendard, size: 10, color: .white)
+    }
+    
+    let customerCenterLabel = UILabel().then {
+        $0.setLabelUI("", font: .pretendard, size: 10, color: .white)
+    }
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
+        setAddView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Set AddView
+    
+    private func setAddView() {
+        [
+            businessNumberLabel,
+            representativeLabel,
+            privacyOfficerLabel,
+            salesLicenseLabel,
+            addressLabel,
+            customerCenterLabel
+        ].forEach{ stackView.addArrangedSubview($0) }
+        
+        [stackView].forEach{ addSubview($0) }
+    }
+    
+    // MARK: - Set Constraints
+    
+    private func setConstraints() {
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Configuration
+    
+    func configureFooter() {
+        let businessInfo = BusinessInfo.businessInfo
+        businessNumberLabel.text = "사업자 번호: \(businessInfo.businessNumber)"
+        representativeLabel.text = "향모아 / 대표자 : \(businessInfo.representativeName)"
+        privacyOfficerLabel.text = "개인정보보호책임자 : \(businessInfo.privacyOfficer)"
+        salesLicenseLabel.text = "통신판매업 : \(businessInfo.salesLicense)"
+        addressLabel.text = "주소 : \(businessInfo.address)"
+        customerCenterLabel.text = "고객센터 : \(businessInfo.customerCenter)"
+    }
 }
